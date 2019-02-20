9a8302f29131b1a61eb6a34d825801417d26cc07
Peter Williams <peter.david.williams.1991@gmail.com>
2019-02-20 16:22:03 +0000

    Added ApprenticeshipInfoServiceMapper

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Mappers/ApprenticeshipInfoServiceMapper.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SFA.DAS.Apprenticeships.Api.Types;
+using SFA.DAS.ProviderCommitments.Domain_Models.ApprenticeshipCourse;
+using SFA.DAS.ProviderCommitments.Interfaces;
+using Framework = SFA.DAS.ProviderCommitments.Domain_Models.ApprenticeshipCourse.Framework;
+using FundingPeriod = SFA.DAS.ProviderCommitments.Domain_Models.ApprenticeshipCourse.FundingPeriod;
+using Standard = SFA.DAS.ProviderCommitments.Domain_Models.ApprenticeshipCourse.Standard;
+
+namespace SFA.DAS.ProviderCommitments.Mappers
+{
+    public class ApprenticeshipInfoServiceMapper : IApprenticeshipInfoServiceMapper
+    {
+        private readonly ICurrentDateTime _currentDateTime;
+
+        public ApprenticeshipInfoServiceMapper(ICurrentDateTime currentDateTime)
+        {
+            _currentDateTime = currentDateTime;
+        }
+
+        public FrameworksView MapFrom(List<FrameworkSummary> frameworks)
+        {
+            return new FrameworksView
+            {
+                CreatedDate = _currentDateTime.Now,
+                Frameworks = frameworks.Select(x => new Framework
+                {
+                    Id = x.Id,
+                    Title = GetTitle(x.FrameworkName.Trim() == x.PathwayName.Trim() ? x.FrameworkName : x.Title, x.Level),
+                    FrameworkCode = x.FrameworkCode,
+                    FrameworkName = x.FrameworkName,
+                    Level = x.Level,
+                    PathwayCode = x.PathwayCode,
+                    PathwayName = x.PathwayName,
+                    Duration = x.Duration,
+                    MaxFunding = x.CurrentFundingCap,
+                    EffectiveFrom = x.EffectiveFrom,
+                    EffectiveTo = x.EffectiveTo,
+                    FundingPeriods = MapFundingPeriods(x.FundingPeriods)
+                }).ToList()
+            };
+        }
+
+        public ProvidersView MapFrom(Apprenticeships.Api.Types.Providers.Provider provider)
+        {
+            return new ProvidersView
+            {
+                CreatedDate = _currentDateTime.Now,
+                Provider = new Provider
+                {
+                    Ukprn = provider.Ukprn,
+                    ProviderName = provider.ProviderName,
+                    Email = provider.Email,
+                    Phone = provider.Phone,
+                    NationalProvider = provider.NationalProvider
+                }
+            };
+        }
+
+        public StandardsView MapFrom(List<StandardSummary> standards)
+        {
+            return new StandardsView
+            {
+                CreationDate = _currentDateTime.Now,
+                Standards = standards.Select(x => new Standard
+                {
+                    Id = x.Id,
+                    Level = x.Level,
+                    Title = GetTitle(x.Title, x.Level) + " (Standard)",
+                    Duration = x.Duration,
+                    MaxFunding = x.CurrentFundingCap,
+                    EffectiveFrom = x.EffectiveFrom,
+                    EffectiveTo = x.LastDateForNewStarts,
+                    FundingPeriods = MapFundingPeriods(x.FundingPeriods)
+                }).ToList()
+            };
+        }
+
+        private static IEnumerable<FundingPeriod> MapFundingPeriods(IEnumerable<Apprenticeships.Api.Types.FundingPeriod> source)
+        {
+            if (source == null)
+                return Enumerable.Empty<FundingPeriod>();
+
+            return source.Select(x => new FundingPeriod
+            {
+                EffectiveFrom = x.EffectiveFrom,
+                EffectiveTo = x.EffectiveTo,
+                FundingCap = x.FundingCap
+            }).OrderBy(y => y.EffectiveFrom ?? DateTime.MinValue);
+        }
+
+        private static string GetTitle(string title, int level)
+        {
+            return $"{title}, Level: {level}";
+        }
+    }
+}
