53b65d5d71cb05b877ccccdefcc59695d47cf27f
Peter Williams <peter.david.williams.1991@gmail.com>
2019-02-21 13:34:59 +0000

    added unit tests

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Tests/Extensions/ITrainingCourseExtensionsTests/WhenDeterminingTheStatusOfACourseOnSpecificDate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.ProviderCommitments.Domain_Models.ApprenticeshipCourse;
+using SFA.DAS.ProviderCommitments.Extensions;
+
+namespace SFA.DAS.ProviderCommitments.Tests.Extensions.ITrainingCourseExtensionsTests
+{
+    [TestFixture]
+    public class WhenDeterminingTheStatusOfACourseOnSpecificDate
+    {
+        [TestCase("2016-01-01", "2016-12-01", "2016-06-01", TrainingCourseStatus.Active, Description = "Within date range")]
+        [TestCase("2016-01-01", "2016-12-01", "2016-01-01", TrainingCourseStatus.Active, Description = "Within date range (on start day)")]
+        [TestCase("2016-01-01", "2016-12-01", "2016-12-01", TrainingCourseStatus.Active, Description = "Within date range (on last day)")]
+        [TestCase("2016-01-15", "2016-12-15", "2016-01-01", TrainingCourseStatus.Active, Description = "Within date range - ignoring start day")]
+        //[TestCase("2016-01-15", "2016-12-15", "2016-12-30", TrainingCourseStatus.Active, Description = "Within date range - ignoring end day")]
+        [TestCase("2016-01-15", "2016-12-15", "2016-12-30", TrainingCourseStatus.Expired, Description = "Past date range (but in same month as courseEnd")]
+        [TestCase(null, "2016-12-01", "2016-06-01", TrainingCourseStatus.Active, Description = "Within date range with no defined course start date")]
+        [TestCase("2016-01-01", null, "2016-06-01", TrainingCourseStatus.Active, Description = "Withing date range, with no defined course end date")]
+        [TestCase(null, null, "2016-06-01", TrainingCourseStatus.Active, Description = "Within date range, with no defined course effective dates")]
+        [TestCase("2016-01-01", "2016-12-01", "2015-06-01", TrainingCourseStatus.Pending, Description = "Outside (before) date range")]
+        [TestCase("2016-01-01", "2016-12-01", "2015-12-31", TrainingCourseStatus.Pending, Description = "Outside (immediately before) date range")]
+        [TestCase("2016-01-01", "2016-12-01", "2017-06-01", TrainingCourseStatus.Expired, Description = "Outside (after) date range")]
+        [TestCase("2016-01-01", "2016-12-01", "2017-01-01", TrainingCourseStatus.Expired, Description = "Outside (immediately after) date range")]
+        [TestCase(null, "2016-12-01", "2017-06-01", TrainingCourseStatus.Expired, Description = "Outside (after) date range with no defined course start date")]
+        public void ThenTheCourseEffectiveDatesAreUsedToDetermineTheStatus(DateTime? courseStart, DateTime? courseEnd, DateTime effectiveDate, TrainingCourseStatus expectStatus)
+        {
+            //Arrange
+            var course = new Mock<ITrainingCourse>();
+            course.SetupGet(x => x.EffectiveFrom).Returns(courseStart);
+            course.SetupGet(x => x.EffectiveTo).Returns(courseEnd);
+
+            //Act
+            var result = course.Object.GetStatusOn(effectiveDate);
+
+            //Assert
+            Assert.AreEqual(expectStatus, result);
+        }
+    }
+}
---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Tests/Queries/GetTrainingCoursesTests/WhenIGetTrainingCourses.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.ProviderCommitments.Domain_Models.ApprenticeshipCourse;
+using SFA.DAS.ProviderCommitments.Interfaces;
+using SFA.DAS.ProviderCommitments.Queries.GetTrainingCourses;
+
+namespace SFA.DAS.ProviderCommitments.Tests.Queries.GetTrainingCoursesTests
+{
+    [TestFixture]
+    public class WhenIGetTrainingCourses
+    {
+        private GetTrainingCoursesQueryHandler _handler;
+
+        private Mock<IApprenticeshipInfoService> _apprenticeshipInfoServiceWrapper;
+
+        private List<Standard> _standards;
+        private List<Framework> _frameworks;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _standards = new List<Standard>
+            {
+                new Standard
+                {
+                    EffectiveFrom = new DateTime(2016,01,01),
+                    EffectiveTo = new DateTime(2016,12,31)
+                }
+            };
+            _frameworks = new List<Framework>
+            {
+                new Framework
+                {
+                    EffectiveFrom = new DateTime(2017,01,01),
+                    EffectiveTo = new DateTime(2017,12,31)
+                }
+            };
+
+            _apprenticeshipInfoServiceWrapper = new Mock<IApprenticeshipInfoService>();
+
+            _apprenticeshipInfoServiceWrapper.Setup(x => x.GetFrameworksAsync(It.IsAny<bool>()))
+                .ReturnsAsync(new FrameworksView
+                {
+                    CreatedDate = DateTime.UtcNow,
+                    Frameworks = _frameworks
+                });
+
+            _apprenticeshipInfoServiceWrapper.Setup(x => x.GetStandardsAsync(It.IsAny<bool>()))
+                .ReturnsAsync(new StandardsView
+                {
+                    CreationDate = DateTime.UtcNow.Date,
+                    Standards = _standards
+                });
+
+            _handler = new GetTrainingCoursesQueryHandler(_apprenticeshipInfoServiceWrapper.Object);
+        }
+
+        [Test]
+        public async Task ThenOnlyStandardsAreReturnedIfIExcludeFrameworks()
+        {
+            var result = await _handler.Handle(new GetTrainingCoursesQueryRequest
+            {
+                IncludeFrameworks = false,
+                EffectiveDate = null
+            }, new CancellationToken());
+
+            Assert.AreEqual(_standards.Count, result.TrainingCourses.Count);
+            Assert.IsInstanceOf<Standard>(result.TrainingCourses[0]);
+        }
+
+        [Test]
+        public async Task ThenStandardsAndFrameworksAreReturnedIfIIncludeFrameworks()
+        {
+            var result = await _handler.Handle(new GetTrainingCoursesQueryRequest
+            {
+                IncludeFrameworks = true,
+                EffectiveDate = null
+            }, new CancellationToken());
+
+            Assert.AreEqual(_standards.Count + _frameworks.Count, result.TrainingCourses.Count);
+            Assert.IsTrue(result.TrainingCourses.Any(x => x is Standard));
+            Assert.IsTrue(result.TrainingCourses.Any(x => x is Framework));
+        }
+
+        [Test]
+        public async Task ThenIfISpecifyAnEffectiveDateIOnlyGetCoursesActiveOnThatDay()
+        {
+            var result = await _handler.Handle(new GetTrainingCoursesQueryRequest
+            {
+                IncludeFrameworks = true,
+                EffectiveDate = new DateTime(2016, 06, 01)
+            }, new CancellationToken());
+
+            Assert.AreEqual(1, result.TrainingCourses.Count);
+            Assert.IsInstanceOf<Standard>(result.TrainingCourses[0]);
+        }
+
+        [Test]
+        public async Task ThenIfIDoNotSpecifyAnEffectiveDateIGetAllCourses()
+        {
+            var result = await _handler.Handle(new GetTrainingCoursesQueryRequest
+            {
+                IncludeFrameworks = true,
+                EffectiveDate = null
+            }, new CancellationToken());
+
+            Assert.AreEqual(2, result.TrainingCourses.Count);
+        }
+    }
+}
