a98a967521492577f2a2e624ebc215e336beadd0
chrisfoster76 <chrisfoster186@googlemail.com>
2019-03-06 09:56:25 +0000

    Adds validation of create cohort request

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Tests/Commands/CreateCohort/WhenICreateCohort.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SFA.DAS.ProviderCommitments.Tests.Commands.CreateCohort
+{
+    [TestFixture]
+    public class WhenICreateCohort
+    {
+        [Test]
+        public void ThenSomething()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Application/Commands/CreateCohort/CreateCohortValidator.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Application/Commands/CreateCohort/CreateCohortValidator.cs
@@ -4,5 +4,11 @@ namespace SFA.DAS.ProviderCommitments.Application.Commands.CreateCohort
 {
     public class CreateCohortValidator : AbstractValidator<CreateCohortRequest>
     {
+        public CreateCohortValidator()
+        {
+            RuleFor(x => x.ProviderId).NotEmpty();
+            RuleFor(x => x.EmployerAccountId).NotEmpty();
+            RuleFor(x => x.LegalEntityId).NotEmpty();
+        }
     }
 }
