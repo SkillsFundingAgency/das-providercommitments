45a1fadf012e4bf2f60514432d85c3b8767bcbad
Paul Graham <paul.graham@amido.com>
2019-03-12 09:51:35 +0000

    renamed test projects to unittests

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.UnitTests/Commands/CreateCohort/WhenCreateCohortRequestIsValidated.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using FluentValidation.Results;
+using NUnit.Framework;
+using SFA.DAS.ProviderCommitments.Application.Commands.CreateCohort;
+
+namespace SFA.DAS.ProviderCommitments.UnitTests.Commands.CreateCohort
+{
+    [TestFixture]
+    public class WhenCreateCohortRequestIsValidated
+    {
+        private CreateCohortValidator _validator;
+        private CreateCohortRequest _validRequest;
+        private Func<ValidationResult> _act;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _validRequest = new CreateCohortRequest
+            {
+                ProviderId = 123,
+                EmployerAccountId = 456,
+                LegalEntityId = "789"
+            };
+
+            _validator = new CreateCohortValidator();
+            _act = () =>_validator.Validate(_validRequest);
+        }
+
+        [Test]
+        public void ThenAValidRequestValidatesSuccessfully()
+        {
+            var result = _act();
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [Test]
+        public void ThenProviderIdIsRequired()
+        {
+            _validRequest.ProviderId = 0;
+            var result = _act();
+            Assert.IsFalse(result.IsValid);
+        }
+
+        [Test]
+        public void ThenEmployerAccountIdIsRequired()
+        {
+            _validRequest.EmployerAccountId = 0;
+            var result = _act();
+            Assert.IsFalse(result.IsValid);
+        }
+
+        [Test]
+        public void ThenLegalEntityIdIsRequired()
+        {
+            _validRequest.LegalEntityId = string.Empty;
+            var result = _act();
+            Assert.IsFalse(result.IsValid);
+        }
+    }
+}
---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.UnitTests/Mappers/ApprenticeshipInfoServiceMapperTests/WhenIMapAProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.ProviderCommitments.Domain_Models.ApprenticeshipCourse;
+using SFA.DAS.ProviderCommitments.Interfaces;
+using SFA.DAS.ProviderCommitments.Mappers;
+
+namespace SFA.DAS.ProviderCommitments.UnitTests.Mappers.ApprenticeshipInfoServiceMapperTests
+{
+    [TestFixture]
+    public class WhenIMapAProvider
+    {
+        private ApprenticeshipInfoServiceMapper _mapper;
+        private Apprenticeships.Api.Types.Providers.Provider _provider;
+        private Func<ProvidersView> _act;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _mapper = new ApprenticeshipInfoServiceMapper(Mock.Of<ICurrentDateTime>());
+
+            _provider = new Apprenticeships.Api.Types.Providers.Provider
+            {
+                Ukprn = 12345678,
+                ProviderName = "TestProvider"
+            };
+
+            _act = () => _mapper.MapFrom(TestHelper.Clone(_provider));
+        }
+
+        [Test]
+        public void ThenUkprnIsMappedCorrectly()
+        {
+            var result = _act();
+
+            Assert.AreEqual(_provider.Ukprn, result.Provider.Ukprn);
+        }
+
+        [Test]
+        public void ThenProviderNameIsMappedCorrectly()
+        {
+            var result = _act();
+
+            Assert.AreEqual(_provider.ProviderName, result.Provider.ProviderName);
+        }
+    }
+}
