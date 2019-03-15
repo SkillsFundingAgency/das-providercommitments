016cb3088806ac7d8b7d760df00e6879efca6e1e
chrisfoster76 <chrisfoster186@googlemail.com>
2019-03-15 09:24:46 +0000

    Removes unused validator

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Models/AddDraftApprenticeshipViewModel.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Models/AddDraftApprenticeshipViewModel.cs
@@ -1,13 +1,10 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-using FluentValidation.Attributes;
 using SFA.DAS.ProviderCommitments.Domain_Models.ApprenticeshipCourse;
 using SFA.DAS.ProviderCommitments.Models;
-using SFA.DAS.ProviderCommitments.Web.Validators;
 
 namespace SFA.DAS.ProviderCommitments.Web.Models
 {
-    [Validator(typeof(AddDraftApprenticeshipViewModelValidator))]
     public class AddDraftApprenticeshipViewModel
     {
         public AddDraftApprenticeshipViewModel()
