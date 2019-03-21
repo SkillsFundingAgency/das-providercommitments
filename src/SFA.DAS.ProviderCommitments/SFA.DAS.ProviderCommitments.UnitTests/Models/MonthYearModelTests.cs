72bd42bd63843db452bbd40f844f142531d069c4
Ian Gledhill <ian@gledhills.mobi>
2019-03-21 17:39:30 +0000

    Save FirstName, LastName, DOB, Start and EndDate

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.UnitTests/Models/MonthYearModelTests.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.UnitTests/Models/MonthYearModelTests.cs
@@ -31,6 +31,34 @@ namespace SFA.DAS.ProviderCommitments.UnitTests.Models
             Assert.Throws<ArgumentException>(() => new MonthYearModel(invalidMonthYear));
         }
 
+        [Test]
+        public void Constructor_WithValidYearMonthElementValues_ShouldMakeDateAvailable()
+        {
+            var date = new MonthYearModel("122019");
+
+            Assert.AreEqual(new DateTime(2019, 12, 1), date.Date);
+        }
+
+        [Test]
+        public void DateTime_WithMonthPartChangedAfterConstruction_ShouldMakeNewDateAvailable()
+        {
+            var date = new MonthYearModel("122019");
+
+            date.Month = 10;
+            Assert.AreEqual(new DateTime(2019, 10, 1), date.Date);
+        }
+
+        [Test]
+        public void DateTime_WithMonthAndYearPartChangedAfterConstruction_ShouldMakeNewDateAvailable()
+        {
+            var date = new MonthYearModel("122019");
+
+            date.Month = 10;
+            date.Year = 2020;
+
+            Assert.AreEqual(new DateTime(2020, 10, 1), date.Date);
+        }
+
         [TestCase("apples")]
         [TestCase("1211111")]
         public void Constructor_WithUnrecogniseableYearMonth_ShouldThrowInvalidArgumentException(string invalidMonthYear)
