176c6866902a2d1e51badf128e2a03685e95284b
Elliot Ritchie <elliotritchie@hotmail.com>
2019-07-18 08:52:28 +0100

    Fixed a bug where not all provider service claims were being checked during authorization

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Authentication/IAuthenticationService.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Authentication/IAuthenticationService.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+
 namespace SFA.DAS.ProviderCommitments.Web.Authentication
 {
     public interface IAuthenticationService
     {
         bool IsUserAuthenticated();
         bool TryGetUserClaimValue(string key, out string value);
+        bool TryGetUserClaimValues(string key, out IEnumerable<string> values);
     }
 }
