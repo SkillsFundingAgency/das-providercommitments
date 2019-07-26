80ec3812004abdb5996fd98dd2ac11c4a36a2839
Elliot Ritchie <elliotritchie@hotmail.com>
2019-07-26 11:36:18 +0100

    Added health checks

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/HealthChecks/CommitmentsApiHealthCheckTests.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Api.Client;
+using SFA.DAS.CommitmentsV2.Api.Types.Responses;
+using SFA.DAS.Http;
+using SFA.DAS.ProviderCommitments.Web.Authentication;
+using SFA.DAS.ProviderCommitments.Web.HealthChecks;
+using SFA.DAS.Testing;
+
+namespace SFA.DAS.ProviderCommitments.Web.UnitTests.HealthChecks
+{
+    [TestFixture]
+    [Parallelizable]
+    public class CommitmentsApiHealthCheckTests : FluentTest<CommitmentsApiHealthCheckTestsFixture>
+    {
+        [TestCase(new [] { Role.Provider }, HealthStatus.Healthy)]
+        [TestCase(new [] { Role.Employer }, HealthStatus.Unhealthy)]
+        [TestCase(new [] { Role.Provider, Role.Employer }, HealthStatus.Unhealthy)]
+        public async Task CheckHealthAsync_WhenWhoAmISucceedsAndUserIsInRoles_ThenShouldReturnHealthStatus(string[] roles, HealthStatus healthStatus)
+        {
+            await TestAsync(
+                f => f.SetWhoAmISuccess(roles),
+                f => f.CheckHealthAsync(),
+                (f, r) =>
+                {
+                    r.Should().NotBeNull();
+                    r.Status.Should().Be(healthStatus);
+                    r.Data["Roles"].Should().BeOfType<List<string>>().Which.Should().BeEquivalentTo(roles);
+                });
+        }
+        
+        [Test]
+        public async Task CheckHealthAsync_WhenWhoAmIFails_ThenShouldThrowException()
+        {
+            await TestExceptionAsync(
+                f => f.SetWhoAmIFailure(),
+                f => f.CheckHealthAsync(),
+                (f, r) => r.Should().Throw<Exception>().Which.Should().Be(f.Exception));
+        }
+    }
+
+    public class CommitmentsApiHealthCheckTestsFixture
+    {
+        public HealthCheckContext HealthCheckContext { get; set; }
+        public Mock<ICommitmentsApiClient> ApiClient { get; set; }
+        public CommitmentsApiHealthCheck HealthCheck { get; set; }
+        public HttpResponseMessage HttpResponseMessage { get; set; }
+        public RestHttpClientException Exception { get; set; }
+
+        public CommitmentsApiHealthCheckTestsFixture()
+        {
+            HealthCheckContext = new HealthCheckContext
+            {
+                Registration = new HealthCheckRegistration("Foo", Mock.Of<IHealthCheck>(), null, null)
+            };
+            
+            ApiClient = new Mock<ICommitmentsApiClient>();
+            HealthCheck = new CommitmentsApiHealthCheck(ApiClient.Object);
+
+            HttpResponseMessage = new HttpResponseMessage(HttpStatusCode.NotFound)
+            {
+                RequestMessage = new HttpRequestMessage(),
+                ReasonPhrase = "Url not found"
+            };
+            
+            Exception = new RestHttpClientException(HttpResponseMessage, "Url not found");
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync()
+        {
+            return HealthCheck.CheckHealthAsync(HealthCheckContext);
+        }
+
+        public CommitmentsApiHealthCheckTestsFixture SetWhoAmISuccess(IEnumerable<string> roles)
+        {
+            ApiClient.Setup(c => c.WhoAmI()).ReturnsAsync(new WhoAmIResponse { Roles = roles.ToList() });
+            
+            return this;
+        }
+
+        public CommitmentsApiHealthCheckTestsFixture SetWhoAmIFailure()
+        {
+            ApiClient.Setup(c => c.WhoAmI()).ThrowsAsync(Exception);
+            
+            return this;
+        }
+    }
+}
---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/HealthChecks/CommitmentsApiHealthCheck.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using SFA.DAS.CommitmentsV2.Api.Client;
+using SFA.DAS.ProviderCommitments.Web.Authentication;
+
+namespace SFA.DAS.ProviderCommitments.Web.HealthChecks
+{
+    public class CommitmentsApiHealthCheck : IHealthCheck
+    {        
+        private readonly ICommitmentsApiClient _apiClient;
+
+        public CommitmentsApiHealthCheck(ICommitmentsApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+        
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            var response = await _apiClient.WhoAmI();
+            var data = new Dictionary<string, object> { ["Roles"] = response.Roles };
+            
+            return response.Roles.Contains(Role.Provider) && !response.Roles.Contains(Role.Employer)
+                ? HealthCheckResult.Healthy(null, data)
+                : HealthCheckResult.Unhealthy(null, null, data);
+        }
+    }
+}
