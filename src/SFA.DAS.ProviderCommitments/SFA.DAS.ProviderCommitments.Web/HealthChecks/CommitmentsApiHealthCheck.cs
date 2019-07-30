340313de6bcc12cac2ba0ef138d67924871edadc
Elliot Ritchie <elliotritchie@hotmail.com>
2019-07-30 11:11:55 +0100

    Updated health check unhealthly description

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/HealthChecks/CommitmentsApiHealthCheck.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/HealthChecks/CommitmentsApiHealthCheck.cs
@@ -23,7 +23,7 @@ namespace SFA.DAS.ProviderCommitments.Web.HealthChecks
             
             return response.Roles.Contains(Role.Provider) && !response.Roles.Contains(Role.Employer)
                 ? HealthCheckResult.Healthy(null, data)
-                : HealthCheckResult.Unhealthy(null, null, data);
+                : HealthCheckResult.Unhealthy($"Client should be in '{Role.Provider}' role and not be in '{Role.Employer}' role", null, data);
         }
     }
 }
