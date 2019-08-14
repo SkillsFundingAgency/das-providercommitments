96a6c1bd82b3cc25618d55cd6fcc0ced0f73c575
chrisfoster186 <chrisfoster186@googlemail.com>
2019-08-14 12:39:25 +0100

    Adds ModelMapper implementation

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.Commitments.Shared/Interfaces/IModelMapper.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.Commitments.Shared.Interfaces
+{
+    public interface IModelMapper
+    {
+        T Map<T>(object source) where T : class;
+    }
+}
---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.Commitments.Shared/Services/ModelMapper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using SFA.DAS.Commitments.Shared.Interfaces;
+
+namespace SFA.DAS.Commitments.Shared.Services
+{
+    public class ModelMapper : IModelMapper
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ModelMapper(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public T Map<T>(object source) where T : class
+        {
+            var sourceType = source.GetType();
+            var destinationType = typeof(T);
+
+            Type[] typeArgs = { sourceType, destinationType };
+            var mapperType = typeof(IMapper<,>).MakeGenericType(typeArgs);
+
+            object mapper;
+
+            try
+            {
+                mapper = _serviceProvider.GetRequiredService(mapperType);
+            }
+            catch (InvalidOperationException e)
+            {
+                throw new InvalidOperationException($"Unable to locate implementation of IMapper<{sourceType.Name},{destinationType.Name}>", e);
+            }
+
+            var mapMethod = mapper.GetType().GetMethod(nameof(IMapper<T, T>.Map));
+            var result = mapMethod.Invoke(mapper, new[] { source });
+
+            return result as T;
+        }
+    }
+}
