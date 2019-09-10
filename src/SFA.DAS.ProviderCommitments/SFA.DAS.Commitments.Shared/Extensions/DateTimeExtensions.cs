1c8dcb8ebc13b956711c77c6417c6f857e65710a
chrisfoster186 <chrisfoster186@googlemail.com>
2019-09-10 12:56:52 +0100

    Adds extension methods

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.Commitments.Shared/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SFA.DAS.Commitments.Shared.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToGdsFormat(this DateTime value)
+        {
+            return $"{value:d MMM yyyy}";
+        }
+        public static string ToGdsFormatWithoutDay(this DateTime value)
+        {
+            return $"{value:MMM yyyy}";
+        }
+    }
+}
---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.Commitments.Shared/Extensions/IntegerExtensionsTests.cs
@@ -0,0 +1,10 @@
+﻿namespace SFA.DAS.Commitments.Shared.Extensions
+{
+    public static class IntegerExtensions
+    {
+        public static string ToGdsCostFormat(this int value)
+        {
+            return $"£{value:n0}";
+        }
+    }
+}
