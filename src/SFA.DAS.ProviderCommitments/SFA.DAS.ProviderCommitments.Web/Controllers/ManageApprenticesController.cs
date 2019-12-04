676a8421ae0fc8e462285728c3a8be56e8c2860c
Peter Williams <peter.david.williams.1991@gmail.com>
2019-12-04 15:58:53 +0000

    added view, model and controller for ManageApprentices

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Controllers/ManageApprenticesController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ProviderCommitments.Web.Models;
+
+namespace SFA.DAS.ProviderCommitments.Web.Controllers
+{
+    [Route("{providerId}/apprentices/manageV2")]
+    public class ManageApprenticesController : Controller
+    {
+        public IActionResult Index(long providerId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var model = new ManageApprenticesViewModel {ProviderId = providerId};
+
+            return View(model);
+        }
+    }
+}
