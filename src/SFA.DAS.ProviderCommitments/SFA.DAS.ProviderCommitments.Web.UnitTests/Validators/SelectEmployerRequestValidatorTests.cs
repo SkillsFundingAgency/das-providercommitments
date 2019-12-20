b74abb5245285cae7da96d81fb35a5792ac0c42f
Akshay Chugh <akshay.chugh30@outlook.com>
2019-12-20 12:03:40 +0000

    CV-512 Request Validator & Unit Tests

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Validators/SelectEmployerRequestValidatorTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+using SFA.DAS.ProviderCommitments.Web.Requests;
+using SFA.DAS.ProviderCommitments.Web.Validators;
+
+namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Validators
+{
+    [TestFixture]
+    public class SelectEmployerRequestValidatorTests 
+    {
+
+        [TestCase(0, false)]
+        [TestCase(1, true)]
+        public void ThenProviderIdIsValidated(long providerId, bool expectedValid)
+        {
+            var request = new SelectEmployerRequest {ProviderId = providerId};
+            AssertValidationResult(x => x.ProviderId, request, expectedValid);
+        }
+
+
+        private void AssertValidationResult<T>(Expression<Func<SelectEmployerRequest, T>> property, SelectEmployerRequest instance, bool expectedValid)
+        {
+            var validator = new SelectEmployerRequestValidator();
+
+            if (expectedValid)
+            {
+                validator.ShouldNotHaveValidationErrorFor(property, instance);
+            }
+            else
+            {
+                validator.ShouldHaveValidationErrorFor(property, instance);
+            }
+        }
+    }
+}
