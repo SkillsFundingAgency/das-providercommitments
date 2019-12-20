8829578721bd13c6842f4f930e99d6cb65919b74
Akshay Chugh <akshay.chugh30@outlook.com>
2019-12-20 11:19:42 +0000

    CV-512 View Work in Progress

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Models/SelectEmployerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SFA.DAS.ProviderRelationships.Types.Dtos;
+
+namespace SFA.DAS.ProviderCommitments.Web.Models
+{
+    public class SelectEmployerViewModel
+    {
+        public IEnumerable<AccountProviderLegalEntityViewModel> AccountProviderLegalEntities { get; set; }
+        public string BackLink { get; set; }
+    }
+}
