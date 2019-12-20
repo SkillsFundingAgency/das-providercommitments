2e0917f00e7c95df8a6679fdc08430f92344cbdc
Akshay Chugh <akshay.chugh30@outlook.com>
2019-12-20 11:20:46 +0000

    CV-512 Request and mapper

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/SelectEmployerViewModelMapper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SFA.DAS.CommitmentsV2.Shared.Interfaces;
+using SFA.DAS.ProviderCommitments.Web.Models;
+using SFA.DAS.ProviderCommitments.Web.Requests;
+using SFA.DAS.ProviderRelationships.Api.Client;
+using SFA.DAS.ProviderRelationships.Types.Dtos;
+using SFA.DAS.ProviderRelationships.Types.Models;
+using SFA.DAS.ProviderUrlHelper;
+
+namespace SFA.DAS.ProviderCommitments.Web.Mappers
+{
+    public class SelectEmployerViewModelMapper : IMapper<SelectEmployerRequest, SelectEmployerViewModel>
+    {
+        private readonly IProviderRelationshipsApiClient _providerRelationshipsApiClient;
+        private readonly ILinkGenerator _linkGenerator;
+
+        public SelectEmployerViewModelMapper(IProviderRelationshipsApiClient providerRelationshipsApiClient, ILinkGenerator linkGenerator)
+        {
+            _providerRelationshipsApiClient = providerRelationshipsApiClient;
+            _linkGenerator = linkGenerator;
+        }
+
+        public async Task<SelectEmployerViewModel> Map(SelectEmployerRequest source)
+        {
+            var result = await _providerRelationshipsApiClient.GetAccountProviderLegalEntitiesWithPermission(
+                new GetAccountProviderLegalEntitiesWithPermissionRequest
+                {
+                    Ukprn = source.ProviderId,
+                    Operation = Operation.CreateCohort
+                });
+
+            var viewModelList = new List<AccountProviderLegalEntityViewModel>();
+            foreach (var apiEntity in result.AccountProviderLegalEntities)
+            {
+                viewModelList.Add(new AccountProviderLegalEntityViewModel
+                {
+                    EmployerAccountLegalEntityName = apiEntity.AccountLegalEntityName,
+                    EmployerAccountLegalEntityPublicHashedId = apiEntity.AccountLegalEntityPublicHashedId,
+                    EmployerAccountName = apiEntity.AccountName,
+                    EmployerAccountPublicHashedId = apiEntity.AccountPublicHashedId
+                });
+            }
+
+            return new SelectEmployerViewModel
+            {
+                AccountProviderLegalEntities = viewModelList,
+                BackLink = _linkGenerator.ProviderApprenticeshipServiceLink($"account")
+            };
+        }
+
+    }
+}
---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Requests/SelectEmployerRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SFA.DAS.Authorization.Context;
+using SFA.DAS.Authorization.ModelBinding;
+using SFA.DAS.ProviderCommitments.Web.Models;
+
+namespace SFA.DAS.ProviderCommitments.Web.Requests
+{
+    public class SelectEmployerRequest
+    {
+        public long ProviderId { get; set; }
+    }
+}
