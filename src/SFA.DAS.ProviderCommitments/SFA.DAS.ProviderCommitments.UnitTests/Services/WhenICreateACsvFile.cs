ba5a7f7f6ed60ea7d809a66f2bd3ef9740c2ca17
Scott Wakefield <scott@expertcc.co.uk>
2020-01-08 17:56:07 +0000

    MF-373 removed unused overload and tests.

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.UnitTests/Services/WhenICreateACsvFile.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.UnitTests/Services/WhenICreateACsvFile.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using CsvHelper;
-using CsvHelper.Configuration;
 using NUnit.Framework;
 using SFA.DAS.ProviderCommitments.Services;
 using SFA.DAS.Testing.AutoFixture;
@@ -24,10 +22,9 @@ namespace SFA.DAS.ProviderCommitments.UnitTests.Services
             var fileString = System.Text.Encoding.Default.GetString(actual);
             var headerLine = fileString.Split(Environment.NewLine)[0];
 
-            Assert.AreEqual(3,headerLine.Split(',').Length);
+            Assert.AreEqual(2,headerLine.Split(',').Length);
             Assert.That(headerLine.Contains(nameof(SomethingToCsv.Id)));
             Assert.That(headerLine.Contains(nameof(SomethingToCsv.Description)));
-            Assert.That(headerLine.Contains(nameof(SomethingToCsv.MoreInternals)));
             Assert.That(!headerLine.Contains(nameof(SomethingToCsv.InternalStuff)));
         }
 
@@ -44,10 +41,9 @@ namespace SFA.DAS.ProviderCommitments.UnitTests.Services
             var fileString = System.Text.Encoding.Default.GetString(actual);
             var lines = fileString.Split(Environment.NewLine);
             Assert.AreEqual(listToWriteToCsv.Count + 2,lines.Length);
-            Assert.AreEqual(3,lines[0].Split(',').Length);
+            Assert.AreEqual(2,lines[0].Split(',').Length);
             Assert.AreEqual(listToWriteToCsv[0].Id.ToString(),lines[1].Split(',')[0]);
             Assert.AreEqual(listToWriteToCsv[0].Description,lines[1].Split(',')[1]);
-            Assert.AreEqual(listToWriteToCsv[0].MoreInternals,lines[1].Split(',')[2]);
         }
 
         [Test, MoqAutoData]
@@ -58,58 +54,6 @@ namespace SFA.DAS.ProviderCommitments.UnitTests.Services
 
             Assert.Throws<WriterException>(() => createCsvService.GenerateCsvContent(nullList));
         }
-
-        [Test, MoqAutoData]
-        public void And_Mapper_Used_Then_Headers_According_To_Mapper(
-            List<SomethingToCsv> listToWriteToCsv,
-            CreateCsvService createCsvService)
-        {
-            var actual = createCsvService.GenerateCsvContent<SomethingToCsv, SomethingToCsvMap>(listToWriteToCsv);
-
-            var fileString = System.Text.Encoding.Default.GetString(actual);
-            var headerLine = fileString.Split(Environment.NewLine)[0];
-
-            Assert.AreEqual(2,headerLine.Split(',').Length);
-            Assert.That(!headerLine.Contains(nameof(SomethingToCsv.MoreInternals)));
-        }
-
-        [Test, MoqAutoData]
-        public void And_Mapper_Used_Then_Maps_List_To_Column(
-            List<SomethingToCsvWithList> listToWriteToCsv,
-            CreateCsvService createCsvService)
-        {
-            var actual = createCsvService.GenerateCsvContent<SomethingToCsvWithList, SomethingToCsvWithListMap>(listToWriteToCsv);
-
-            var fileString = System.Text.Encoding.Default.GetString(actual);
-            var lines = fileString.Split(Environment.NewLine);
-            var headerLine = lines[0];
-
-            Assert.AreEqual(4,headerLine.Split(',').Length);
-            Assert.That(headerLine.Contains(nameof(SomethingToCsvWithList.NastyList)));
-            Assert.AreEqual(listToWriteToCsv[0].NastyList.Aggregate((a,b) => $"{a}|{b}"),lines[1].Split(',')[3]);
-        }
-
-        [Test, MoqAutoData]
-        public void And_Mapper_Used_Then_Maps_List_Of_Single_Value_To_Column(
-            List<SomethingToCsvWithList> listToWriteToCsv,
-            string singleItem,
-            CreateCsvService createCsvService)
-        {
-            foreach (var somethingToCsvWithList in listToWriteToCsv)
-            {
-                somethingToCsvWithList.NastyList = new List<string>{singleItem};
-            }
-
-            var actual = createCsvService.GenerateCsvContent<SomethingToCsvWithList, SomethingToCsvWithListMap>(listToWriteToCsv);
-
-            var fileString = System.Text.Encoding.Default.GetString(actual);
-            var lines = fileString.Split(Environment.NewLine);
-            var headerLine = lines[0];
-
-            Assert.AreEqual(4,headerLine.Split(',').Length);
-            Assert.That(headerLine.Contains(nameof(SomethingToCsvWithList.NastyList)));
-            Assert.AreEqual(singleItem,lines[1].Split(',')[3]);
-        }
     }
 
     public class SomethingToCsv
@@ -118,29 +62,5 @@ namespace SFA.DAS.ProviderCommitments.UnitTests.Services
         public string Description { get; set; }
         [CsvHelper.Configuration.Attributes.Ignore]
         public long InternalStuff { get; set; }
-        public string MoreInternals { get; set; }
     }
-
-    public class SomethingToCsvMap : ClassMap<SomethingToCsv>
-    {
-        public SomethingToCsvMap()
-        {
-            AutoMap();
-            Map(m => m.MoreInternals).Ignore();
-        }
-    }
-
-    public class SomethingToCsvWithList : SomethingToCsv
-    {
-        public IEnumerable<string> NastyList { get; set; }
-    }
-
-    public class SomethingToCsvWithListMap : ClassMap<SomethingToCsvWithList>
-    {
-        public SomethingToCsvWithListMap()
-        {
-            AutoMap();
-            Map(m => m.NastyList).ConvertUsing(m => m.NastyList.Aggregate((a, b) => $"{a}|{b}"));
-        }
-    } 
 }
--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Services/CreateCsvService.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Services/CreateCsvService.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using CsvHelper;
-using CsvHelper.Configuration;
 
 namespace SFA.DAS.ProviderCommitments.Services
 {
@@ -15,24 +14,6 @@ namespace SFA.DAS.ProviderCommitments.Services
                 {
                     using (var csvWriter = new CsvWriter(streamWriter))
                     {
-                        csvWriter.WriteRecords(results);
-                        streamWriter.Flush();
-                        memoryStream.Position = 0;
-                        return memoryStream.ToArray();
-                    }
-                }
-            }
-        }
-
-        public byte[] GenerateCsvContent<T, TMap>(IEnumerable<T> results) where TMap : ClassMap<T>
-        {
-            using (var memoryStream = new MemoryStream())
-            {
-                using (var streamWriter = new StreamWriter(memoryStream))
-                {
-                    using (var csvWriter = new CsvWriter(streamWriter))
-                    {
-                        csvWriter.Configuration.RegisterClassMap<TMap>();
                         csvWriter.WriteRecords(results);
                         streamWriter.Flush();
                         memoryStream.Position = 0;
