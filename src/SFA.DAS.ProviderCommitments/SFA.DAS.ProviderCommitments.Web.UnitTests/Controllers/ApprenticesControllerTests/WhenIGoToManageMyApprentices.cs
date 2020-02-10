d71e17baafc17417666b840e80b01a2b4f676b56
Scott Wakefield <scott@expertcc.co.uk>
2020-02-10 16:47:43 +0000

    MF-472 added missing test.

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Controllers/ApprenticesControllerTests/WhenIGoToManageMyApprentices.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Controllers/ApprenticesControllerTests/WhenIGoToManageMyApprentices.cs
@@ -35,5 +35,26 @@ namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Controllers.ApprenticesContr
             //Assert
             actualModel.Should().BeEquivalentTo(expectedViewModel);
         }
+
+        [Test, MoqAutoData]
+        public async Task Then_SortedByHeaderClassName_Set(
+            IndexRequest request,
+            IndexViewModel expectedViewModel,
+            [Frozen] Mock<IModelMapper> apprenticeshipMapper,
+            ApprenticeController controller)
+        {
+            //Arrange
+            expectedViewModel.FilterModel.ReverseSort = false;
+            apprenticeshipMapper
+                .Setup(mapper => mapper.Map<IndexViewModel>(request))
+                .ReturnsAsync(expectedViewModel);
+
+            //Act
+            var result = await controller.Index(request) as ViewResult;
+            var actualModel = result.Model as IndexViewModel;
+
+            //Assert
+            actualModel.SortedByHeaderClassName.Should().EndWith("das-table__sort--asc");
+        }
     }
 }
