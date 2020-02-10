a5f294cd9404d620d859bd2af8e734238e149db8
Akshay Chugh <akshay.chugh30@outlook.com>
2020-02-10 11:00:20 +0000

    CV-587 Inform Request & ViewModel

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Models/Apprentice/InformRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SFA.DAS.Authorization.ModelBinding;
+
+namespace SFA.DAS.ProviderCommitments.Web.Models.Apprentice
+{
+    public class InformRequest : DetailsRequest
+    {
+    }
+}
