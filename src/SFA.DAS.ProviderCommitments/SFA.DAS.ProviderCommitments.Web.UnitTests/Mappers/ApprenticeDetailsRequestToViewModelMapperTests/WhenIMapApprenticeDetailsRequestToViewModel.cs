31a8f7ce9bed51a3670466c77e89a1f4fd367c68
Akshay Chugh <akshay.chugh30@outlook.com>
2020-02-24 11:01:26 +0000

    CV-586 Updated: DetailsViewModelMapper + tests

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Mappers/ApprenticeDetailsRequestToViewModelMapperTests/WhenIMapApprenticeDetailsRequestToViewModel.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Mappers/ApprenticeDetailsRequestToViewModelMapperTests/WhenIMapApprenticeDetailsRequestToViewModel.cs
@@ -8,8 +8,11 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using SFA.DAS.Authorization.Features.Services;
+using SFA.DAS.Authorization.ProviderFeatures.Models;
 using SFA.DAS.CommitmentsV2.Api.Types.Responses;
 using SFA.DAS.Encoding;
+using SFA.DAS.ProviderCommitments.Features;
 using SFA.DAS.ProviderCommitments.Web.Mappers.Apprentice;
 using SFA.DAS.ProviderCommitments.Web.Models.Apprentice;
 
@@ -124,6 +127,17 @@ namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Mappers.ApprenticeDetailsReq
             Assert.AreEqual(_fixture.PriceEpisodesApiResponse.PriceEpisodes.First().Cost, _fixture.Result.Cost);
         }
 
+        [TestCase(true)]
+        [TestCase(false)]
+        public async Task ThenChangeOfEmployerEnabledIsMappedCorrectly(bool enabled)
+        {
+            _fixture.WithChangeOfEmployerToggle(enabled);
+
+            await _fixture.Map();
+
+            Assert.AreEqual(enabled, _fixture.Result.ChangeOfEmployerEnabled);
+        }
+
         public class WhenIMapApprenticeDetailsRequestToViewModelFixture
         {
             private readonly DetailsViewModelMapper _mapper;
@@ -133,6 +147,7 @@ namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Mappers.ApprenticeDetailsReq
             public GetPriceEpisodesResponse PriceEpisodesApiResponse { get; }
             
             private readonly Mock<IEncodingService> _encodingService;
+            private readonly Mock<IFeatureTogglesService<ProviderFeatureToggle>> _featureToggleService;
             public string CohortReference { get; }
             public string AgreementId { get; }
 
@@ -155,6 +170,16 @@ namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Mappers.ApprenticeDetailsReq
                 _encodingService.Setup(x => x.Encode(It.IsAny<long>(), EncodingType.CohortReference)).Returns(CohortReference);
                 _encodingService.Setup(x => x.Encode(It.IsAny<long>(), EncodingType.PublicAccountLegalEntityId)).Returns(AgreementId);
 
+                _featureToggleService = new Mock<IFeatureTogglesService<ProviderFeatureToggle>>();
+                _featureToggleService
+                    .Setup(x => x.GetFeatureToggle(It.IsAny<string>()))
+                    .Returns(new ProviderFeatureToggle()
+                    {
+                        Feature = nameof(ProviderFeature.ChangeOfEmployer),
+                        IsEnabled = false,
+                        Whitelist = null
+                    });
+
                 var apiClient = new Mock<ICommitmentsApiClient>();
                 apiClient.Setup(x => x.GetApprenticeship(It.IsAny<long>(), It.IsAny<CancellationToken>()))
                     .ReturnsAsync(ApiResponse);
@@ -162,12 +187,25 @@ namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Mappers.ApprenticeDetailsReq
                 apiClient.Setup(x => x.GetPriceEpisodes(It.IsAny<long>(), It.IsAny<CancellationToken>()))
                     .ReturnsAsync(PriceEpisodesApiResponse);
 
-                _mapper = new DetailsViewModelMapper(apiClient.Object, _encodingService.Object);
+                _mapper = new DetailsViewModelMapper(apiClient.Object, _encodingService.Object, _featureToggleService.Object);
             }
 
             public async Task<WhenIMapApprenticeDetailsRequestToViewModelFixture> Map()
             {
                 Result = await _mapper.Map(Source);
+                return this;
+            }
+
+            public WhenIMapApprenticeDetailsRequestToViewModelFixture WithChangeOfEmployerToggle(bool enabled)
+            {
+                _featureToggleService
+                    .Setup(x => x.GetFeatureToggle(nameof(ProviderFeature.ChangeOfEmployer)))
+                    .Returns(new ProviderFeatureToggle()
+                    {
+                        Feature = nameof(ProviderFeature.ChangeOfEmployer),
+                        IsEnabled = enabled,
+                        Whitelist = null
+                    });
                 return this;
             }
         }
--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/Apprentice/DetailsViewModelMapper.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/Apprentice/DetailsViewModelMapper.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using SFA.DAS.Authorization.Features.Services;
 using SFA.DAS.CommitmentsV2.Api.Client;
 using SFA.DAS.CommitmentsV2.Shared.Interfaces;
 using SFA.DAS.Encoding;
 using SFA.DAS.ProviderCommitments.Web.Extensions;
 using SFA.DAS.ProviderCommitments.Web.Models.Apprentice;
+using System.Threading.Tasks;
+using SFA.DAS.Authorization.ProviderFeatures.Models;
+using SFA.DAS.ProviderCommitments.Features;
 
 namespace SFA.DAS.ProviderCommitments.Web.Mappers.Apprentice
 {
@@ -12,11 +14,14 @@ namespace SFA.DAS.ProviderCommitments.Web.Mappers.Apprentice
     {
         private readonly ICommitmentsApiClient _commitmentApiClient;
         private readonly IEncodingService _encodingService;
+        private readonly IFeatureTogglesService<ProviderFeatureToggle> _featureTogglesService;
 
-        public DetailsViewModelMapper(ICommitmentsApiClient commitmentApiClient, IEncodingService encodingService)
+        public DetailsViewModelMapper(ICommitmentsApiClient commitmentApiClient, IEncodingService encodingService,
+            IFeatureTogglesService<ProviderFeatureToggle> featureTogglesService)
         {
             _commitmentApiClient = commitmentApiClient;
             _encodingService = encodingService;
+            _featureTogglesService = featureTogglesService;
         }
 
         public async Task<DetailsViewModel> Map(DetailsRequest source)
@@ -24,6 +29,8 @@ namespace SFA.DAS.ProviderCommitments.Web.Mappers.Apprentice
             var detailsResponseTask = _commitmentApiClient.GetApprenticeship(source.ApprenticeshipId);
             var priceEpisodes = await _commitmentApiClient.GetPriceEpisodes(source.ApprenticeshipId);
             var detailsResponse = await detailsResponseTask;
+
+            var changeOfEmployerEnabled = _featureTogglesService.GetFeatureToggle(nameof(ProviderFeature.ChangeOfEmployer))?.IsEnabled ?? false;
 
             return new DetailsViewModel
             {
@@ -41,7 +48,8 @@ namespace SFA.DAS.ProviderCommitments.Web.Mappers.Apprentice
                 StartDate = detailsResponse.StartDate,
                 EndDate = detailsResponse.EndDate,
                 ProviderRef = detailsResponse.Reference,
-                Cost = priceEpisodes.PriceEpisodes.GetPrice()
+                Cost = priceEpisodes.PriceEpisodes.GetPrice(),
+                ChangeOfEmployerEnabled = changeOfEmployerEnabled
             };
         }
     }
