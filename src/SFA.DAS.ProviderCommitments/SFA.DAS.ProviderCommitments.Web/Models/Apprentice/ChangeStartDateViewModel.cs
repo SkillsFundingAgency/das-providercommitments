e6f7d8e27a80d0550fbc64fb559c016df7289b05
Akshay Chugh <akshay.chugh30@outlook.com>
2020-03-05 11:51:37 +0000

    CV-590 Refactored model names + initial controller

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Models/Apprentice/ChangeStartDateViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SFA.DAS.Authorization.ModelBinding;
+using SFA.DAS.CommitmentsV2.Shared.Models;
+
+namespace SFA.DAS.ProviderCommitments.Web.Models.Apprentice
+{
+    public class ChangeStartDateViewModel : IAuthorizationContextModel
+    {
+        public ChangeStartDateViewModel()
+        {
+            StartDate = new MonthYearModel("");
+        }
+
+        public long ProviderId { get; set; }
+        public string EmployerAccountLegalEntityPublicHashedId { get; set; }
+        public long AccountLegalEntityId { get; set; }
+        public string ApprenticeshipHashedId { get; set; }
+        public long ApprenticeshipId { get; set; }
+        public DateTime? StopDate { get; set; }
+        public MonthYearModel StartDate { get; set; }
+    }
+}
