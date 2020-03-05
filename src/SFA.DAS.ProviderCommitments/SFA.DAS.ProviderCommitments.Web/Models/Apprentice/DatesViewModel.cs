742326b54be0afcd149432d5d9b1c4e088f768b1
Akshay Chugh <akshay.chugh30@outlook.com>
2020-03-05 09:53:51 +0000

    CV-590 Initial DatesViewModel

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Models/Apprentice/DatesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SFA.DAS.Authorization.ModelBinding;
+
+namespace SFA.DAS.ProviderCommitments.Web.Models.Apprentice
+{
+    public class DatesViewModel : IAuthorizationContextModel
+    {
+        public long ProviderId { get; set; }
+        public string EmployerAccountLegalEntityPublicHashedId { get; set; }
+        public long AccountLegalEntityId { get; set; }
+        public string ApprenticeshipHashedId { get; set; }
+        public long ApprenticeshipId { get; set; }
+        public DateTime? StopDate { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
