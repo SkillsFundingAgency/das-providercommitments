a027224619bf96e1dc609d48685c77a6770ee1a2
Akshay Chugh <akshay.chugh30@outlook.com>
2020-03-06 15:47:35 +0000

    CV-590 Initial ViewModel mapper

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/Apprentice/ChangeStartDateViewModelMapper.cs
@@ -0,0 +1,30 @@
+﻿using SFA.DAS.CommitmentsV2.Shared.Interfaces;
+using SFA.DAS.ProviderCommitments.Web.Models.Apprentice;
+using System.Threading.Tasks;
+using SFA.DAS.CommitmentsV2.Api.Client;
+
+namespace SFA.DAS.ProviderCommitments.Web.Mappers.Apprentice
+{
+    public class ChangeStartDateViewModelMapper : IMapper<ChangeStartDateRequest, ChangeStartDateViewModel>
+    {
+        private readonly ICommitmentsApiClient _commitmentsApiClient;
+
+        public ChangeStartDateViewModelMapper(ICommitmentsApiClient commitmentsApiClient)
+        {
+            _commitmentsApiClient = commitmentsApiClient;
+        }
+
+        public async Task<ChangeStartDateViewModel> Map(ChangeStartDateRequest source)
+        {
+            var apprenticeship = await _commitmentsApiClient.GetApprenticeship(source.ApprenticeshipId);
+
+            return new ChangeStartDateViewModel
+            {
+                ApprenticeshipHashedId = source.ApprenticeshipHashedId,
+                EmployerAccountLegalEntityPublicHashedId = source.EmployerAccountLegalEntityPublicHashedId,
+                StopDate = apprenticeship.StopDate,
+                ProviderId = source.ProviderId
+            };
+        }
+    }
+}
