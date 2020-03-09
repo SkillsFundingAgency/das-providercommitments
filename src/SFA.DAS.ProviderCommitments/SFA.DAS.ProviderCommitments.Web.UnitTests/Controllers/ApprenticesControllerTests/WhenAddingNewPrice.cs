59c0f390960d28c2e384351ff6fbeb7ae5da0a02
Paul Graham <paul.graham@coprime.co.uk>
2020-03-09 09:58:23 +0000

    added controller tests

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Controllers/ApprenticesControllerTests/WhenAddingNewPrice.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Shared.Interfaces;
+using SFA.DAS.ProviderCommitments.Web.Controllers;
+using SFA.DAS.ProviderCommitments.Web.Models.Apprentice;
+using SFA.DAS.ProviderCommitments.Web.Models.Shared;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoFixture;
+using SFA.DAS.ProviderCommitments.Web.RouteValues;
+
+namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Controllers.ApprenticesControllerTests
+{
+    [TestFixture]
+    public class WhenAddingNewPrice
+    {
+        [Test]
+        public async Task GetThenCallsChangePriceViewModelMapper()
+        {
+            var fixture = new WhenAddingNewPriceFixture();
+
+            await fixture.Sut.ChangePrice(fixture.ChangePriceRequest);
+
+            fixture.VerifyChangePriceViewMapperWasCalled();
+        }
+
+        [Test]
+        public async Task GetThenReturnsView()
+        {
+            var fixture = new WhenAddingNewPriceFixture();
+
+            var result = await fixture.Sut.ChangePrice(fixture.ChangePriceRequest) as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.AreEqual(typeof(ChangePriceViewModel), result.Model.GetType());
+        }
+
+        [Test]
+        public async Task PostThenCallsChangeOfEmployerRequestModelMapper()
+        {
+            var fixture = new WhenAddingNewPriceFixture();
+
+            await fixture.Sut.ChangePrice(fixture.ChangePriceViewModel);
+
+            fixture.VerifyChangeOfEmployerMapperWasCalled();
+        }
+
+        [Test]
+        public async Task PostThenReturnsARedirectResult()
+        {
+            var fixture = new WhenAddingNewPriceFixture();
+
+            var result = await fixture.Sut.ChangePrice(fixture.ChangePriceViewModel) as RedirectToRouteResult;
+
+            Assert.NotNull(result);
+            Assert.AreEqual(RouteNames.ApprenticeConfirmChangeOfEmployer, result.RouteName);
+        }
+    }
+
+    public class WhenAddingNewPriceFixture
+    {
+        public ApprenticeController Sut { get; set; }
+        public ChangePriceRequest ChangePriceRequest { get; set; }
+        public ChangePriceViewModel ChangePriceViewModel { get; set; }
+
+        private readonly Mock<IModelMapper> _modelMapperMock;
+        private readonly Fixture _fixture;
+
+        public WhenAddingNewPriceFixture()
+        {
+            _fixture = new Fixture();
+            ChangePriceRequest = _fixture.Create<ChangePriceRequest>();
+            ChangePriceViewModel = _fixture.Create<ChangePriceViewModel>();
+
+            _modelMapperMock = new Mock<IModelMapper>();
+            _modelMapperMock.Setup(x => x.Map<ChangePriceViewModel>(It.IsAny<ChangePriceRequest>()))
+                .ReturnsAsync(ChangePriceViewModel);
+
+            Sut = new ApprenticeController(_modelMapperMock.Object);
+        }
+
+        public void VerifyChangePriceViewMapperWasCalled()
+        {
+            _modelMapperMock.Verify(x => x.Map<ChangePriceViewModel>(ChangePriceRequest));
+        }
+
+        public void VerifyChangeOfEmployerMapperWasCalled()
+        {
+            _modelMapperMock.Verify(x => x.Map<ChangeOfEmployerRequest>(ChangePriceViewModel));
+        }
+    }
+}
--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Mappers/Apprentice/ChangeOfEmployerRequestMapperTests.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Mappers/Apprentice/ChangeOfEmployerRequestMapperTests.cs
@@ -1,11 +1,9 @@
 ﻿using AutoFixture;
 using Moq;
 using NUnit.Framework;
-using SFA.DAS.CommitmentsV2.Api.Client;
 using SFA.DAS.ProviderCommitments.Web.Mappers.Apprentice;
 using SFA.DAS.ProviderCommitments.Web.Models.Apprentice;
 using System;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
@@ -65,7 +63,7 @@ namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Mappers.Apprentice
         }
 
         [Test]
-        public async Task ThenThrowsExceptionWhenNewPriceIsNull()
+        public void ThenThrowsExceptionWhenNewPriceIsNull()
         {
             _source.NewPrice = null;
             Assert.ThrowsAsync<InvalidOperationException>( () => _mapper.Map(TestHelper.Clone(_source)));
