8d116e122f9ba8c71fb31379349819a631f313dd
Akshay Chugh <akshay.chugh30@outlook.com>
2020-03-10 07:56:32 +0000

    CV-590 Refactor: Renamed variables and files

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Models/Apprentice/PriceRequest.cs
@@ -0,0 +1,12 @@
+﻿using SFA.DAS.CommitmentsV2.Shared.Models;
+
+namespace SFA.DAS.ProviderCommitments.Web.Models.Apprentice
+{
+    public class PriceRequest
+    {
+        public long ProviderId { get; set; }
+        public string EmployerAccountLegalEntityPublicHashedId { get; set; }
+        public string ApprenticeshipHashedId { get; set; }
+        public string NewStartDate { get; set; }
+    }
+}
