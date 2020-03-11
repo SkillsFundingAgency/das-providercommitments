9a72bd62fd23d4992991c058f88354c27fbf4a55
Paul Graham <paul.graham@coprime.co.uk>
2020-03-11 14:11:46 +0000

    added controller unit test

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Controllers/ApprenticesControllerTests/WhenConfirmingChangeOfEmployer.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Shared.Interfaces;
+using SFA.DAS.ProviderCommitments.Web.Controllers;
+using SFA.DAS.ProviderCommitments.Web.Models.Apprentice;
+using System.Threading.Tasks;
+using AutoFixture;
+
+namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Controllers.ApprenticesControllerTests
+{
+    [TestFixture]
+    public class WhenConfirmingChangeOfEmployer
+    {
+        [Test]
+        public async Task GetThenCallsPriceViewModelMapper()
+        {
+            var fixture = new WhenConfirmingChangeOfEmployerFixture();
+
+            await fixture.Sut.ConfirmChangeOfEmployer(fixture.ChangeOfEmployerRequest);
+
+            fixture.VerifyChangeOfRequestViewMapperWasCalled();
+        }
+
+        [Test]
+        public async Task GetThenReturnsView()
+        {
+            var fixture = new WhenConfirmingChangeOfEmployerFixture();
+
+            var result = await fixture.Sut.ConfirmChangeOfEmployer(fixture.ChangeOfEmployerRequest) as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.AreEqual(typeof(ChangeOfEmployerViewModel), result.Model.GetType());
+        }
+    }
+
+    public class WhenConfirmingChangeOfEmployerFixture
+    {
+        public ApprenticeController Sut { get; set; }
+        public ChangeOfEmployerRequest ChangeOfEmployerRequest { get; set; }
+        public ChangeOfEmployerViewModel ChangeOfEmployerViewModel { get; set; }
+
+        private readonly Mock<IModelMapper> _modelMapperMock;
+        private readonly Fixture _fixture;
+
+        public WhenConfirmingChangeOfEmployerFixture()
+        {
+            _fixture = new Fixture();
+            ChangeOfEmployerRequest = _fixture.Create<ChangeOfEmployerRequest>();
+            ChangeOfEmployerViewModel = _fixture.Create<ChangeOfEmployerViewModel>();
+
+            _modelMapperMock = new Mock<IModelMapper>();
+            _modelMapperMock.Setup(x => x.Map<ChangeOfEmployerViewModel>(It.IsAny<ChangeOfEmployerRequest>()))
+                .ReturnsAsync(ChangeOfEmployerViewModel);
+
+            Sut = new ApprenticeController(_modelMapperMock.Object);
+        }
+
+        public void VerifyChangeOfRequestViewMapperWasCalled()
+        {
+            _modelMapperMock.Verify(x => x.Map<ChangeOfEmployerViewModel>(ChangeOfEmployerRequest));
+        }
+    }
+}
