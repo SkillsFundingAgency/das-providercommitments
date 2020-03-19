52aa8126841354fa27a0ad1205ae024a84e0681d
Shahzad <shahzad.khan@digital.education.gov.uk>
2020-03-19 15:47:12 +0000

    CV-596 removed commented code.

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/Apprentice/ChangeOfEmployerViewModelMapper.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/Apprentice/ChangeOfEmployerViewModelMapper.cs
@@ -39,7 +39,7 @@ namespace SFA.DAS.ProviderCommitments.Web.Mappers.Apprentice
                     EmployerAccountLegalEntityPublicHashedId = source.EmployerAccountLegalEntityPublicHashedId,
                     OldEmployerName = data.Apprenticeship.EmployerName,
                     ApprenticeName = $"{data.Apprenticeship.FirstName} {data.Apprenticeship.LastName}",
-                    StopDate =   data.Apprenticeship.StopDate.Value, //DateTime.Now.AddDays(-199),
+                    StopDate =   data.Apprenticeship.StopDate.Value, 
                     OldStartDate = data.Apprenticeship.StartDate,
                     OldPrice = decimal.ToInt32(data.PriceEpisodes.PriceEpisodes.GetPrice()), 
                     NewEmployerName = data.AccountLegalEntity.LegalEntityName, 
