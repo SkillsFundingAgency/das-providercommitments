5363d714fd2d2583e5c1f737c9103862216539cb
Paul Howes <paul.howes@cognetics.net>
2020-08-25 11:58:30 +0100

    Fixes for validation

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Models/Apprentice/PriceViewModel.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Models/Apprentice/PriceViewModel.cs
@@ -13,7 +13,7 @@ namespace SFA.DAS.ProviderCommitments.Web.Models.Apprentice
         public string StartDate { get; set; }
         public string EndDate { get; set; }
         [SuppressArgumentException(nameof(Price), "Total agreed apprenticeship price must be 7 numbers or fewer")]
-        public decimal? Price { get; set; }
+        public int? Price { get; set; }
         public bool InEditMode { get; set; }
     }
 }
