af37bc35ab0c803d96a1a10ed0bddb917f5289b5
Corey <corey.faulconbridge@digital.education.gov.uk>
2021-11-26 15:13:14 +0000

    [CON-4188] Add validator

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Validators/Cohort/SelectDraftApprenticeshipsEntryMethodViewModelValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using SFA.DAS.ProviderCommitments.Web.Models.Cohort;
+
+namespace SFA.DAS.ProviderCommitments.Web.Validators.Cohort
+{
+    public class SelectDraftApprenticeshipsEntryMethodViewModelValidator : AbstractValidator<SelectAddDraftApprenticeshipJourneyViewModel>
+    {
+        public SelectDraftApprenticeshipsEntryMethodViewModelValidator()
+        {
+            RuleFor(x => x.Selection).NotNull().WithMessage("Select how you want to add apprentices");
+        }
+    }
+}
