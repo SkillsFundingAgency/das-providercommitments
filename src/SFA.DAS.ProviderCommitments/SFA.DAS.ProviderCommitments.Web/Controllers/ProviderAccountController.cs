eb360d0011cab6c1ce7efc16c2271d66c45c9a80
Paul Howes <paul.howes@cognetics.net>
2021-12-06 12:52:59 +0000

    Update ProviderAccountController.cs

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Controllers/ProviderAccountController.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Controllers/ProviderAccountController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.WsFederation;
 using Microsoft.AspNetCore.Mvc;
-using SFA.DAS.Provider.Shared.UI.Models;
 using SFA.DAS.ProviderCommitments.Web.RouteValues;
 
 namespace SFA.DAS.ProviderCommitments.Web.Controllers
@@ -9,20 +8,13 @@ namespace SFA.DAS.ProviderCommitments.Web.Controllers
     [Route("{providerId}")]
     public class ProviderAccountController : Controller
     {
-        private readonly ProviderSharedUIConfiguration _providerSharedUiConfiguration;
-
-        public ProviderAccountController(ProviderSharedUIConfiguration providerSharedUiConfiguration)
-        {
-            _providerSharedUiConfiguration = providerSharedUiConfiguration;
-        }
-
         [Route("signout", Name = RouteNames.ProviderSignOut)]
         public IActionResult SignOut()
         {
             return SignOut(
                 new Microsoft.AspNetCore.Authentication.AuthenticationProperties
                 {
-                    RedirectUri = $"{_providerSharedUiConfiguration.DashboardUrl}/account",
+                    RedirectUri = "",
                     AllowRefresh = true
                 },
                 CookieAuthenticationDefaults.AuthenticationScheme,
