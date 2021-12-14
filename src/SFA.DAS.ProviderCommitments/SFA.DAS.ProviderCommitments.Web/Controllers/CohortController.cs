acdf01915d2cd0b5b2fca9b017997640b250f1a3
Paul Howes <paul.howes@cognetics.net>
2021-12-14 12:37:20 +0000

    Update CohortController.cs

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Controllers/CohortController.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Controllers/CohortController.cs
@@ -64,6 +64,8 @@ namespace SFA.DAS.ProviderCommitments.Web.Controllers
         public async Task<IActionResult> Draft(CohortsByProviderRequest request)
         {
             var draftViewModel = await _modelMapper.Map<DraftViewModel>(request);
+            draftViewModel.SortedByHeader();
+
             return View(draftViewModel);
         }
 
