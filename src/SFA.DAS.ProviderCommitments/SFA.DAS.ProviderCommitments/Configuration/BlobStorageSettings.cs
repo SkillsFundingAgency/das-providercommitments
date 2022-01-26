56f8d70838ba14ad755d7c01f9454814094374d3
Shahzad <hkf-tech@users.noreply.github.com>
2022-01-26 14:13:56 +0000

    merge latest master

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Configuration/BlobStorageSettings.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Configuration/BlobStorageSettings.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SFA.DAS.ProviderCommitments.Configuration
+﻿namespace SFA.DAS.ProviderCommitments.Configuration
 {
     public class BlobStorageSettings
     {
