5fc77e2f37ad7706f8b7b6a9a89d170388201b5e
Shahzad <hkf-tech@users.noreply.github.com>
2022-02-07 16:51:41 +0000

    adding files

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/LocalDevRegistry/ToRemove/CommitmentApiClient2.cs
@@ -0,0 +1,26 @@
+﻿using SFA.DAS.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ProviderCommitments.Web.LocalDevRegistry.ToRemove
+{
+    public class CommitmentApiClient2 : CommitmentsV2.Api.Client.CommitmentsApiClient
+    {
+        private readonly IRestHttpClient _client;
+
+        public CommitmentApiClient2(IRestHttpClient client) :base(client)
+        {
+            _client = client;
+        }
+
+
+        public Task ValidateBulkUploadRequest(long providerId, BulkUploadValidateRequest request, CancellationToken cancellationToken = default)
+        {
+            return _client.PostAsJson($"api/{providerId}/bulkupload/validate", request, cancellationToken);
+        }
+
+    }
+}
---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Models/Cohort/FileUploadValidateRequest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ProviderCommitments.Web.Models.Cohort
+{
+    public class FileUploadValidateRequest
+    {
+        public long ProviderId { get; set; }
+        public IFormFile  Attachement { get; set; }
+    }
+}
