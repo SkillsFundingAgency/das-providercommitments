567d5344a908464c297964fccb24da3bb17cb90c
Shahzad <hkf-tech@users.noreply.github.com>
2022-03-08 16:01:59 +0000

    code refactoring

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Mappers/Cohort/WhenMappingBulkUploadApiResponseToFileUpldValidateViewModelTests.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Mappers/Cohort/WhenMappingBulkUploadApiResponseToFileUpldValidateViewModelTests.cs
@@ -47,35 +47,35 @@ namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Mappers.Cohort
         [Test]
         public async Task ApprenticeName_Is_Mapped()
         {
-            Assert.AreEqual(_source.BulkUploadValidationErrors.First().ApprenticeName, _result.BulkUploadValidationErrors.First().ApprenticeName);
-            Assert.AreEqual(_source.BulkUploadValidationErrors.Last().ApprenticeName, _result.BulkUploadValidationErrors.Last().ApprenticeName);
+            Assert.AreEqual(_source.First().ApprenticeName, _result.BulkUploadValidationErrors.First().ApprenticeName);
+            Assert.AreEqual(_source.Last().ApprenticeName, _result.BulkUploadValidationErrors.Last().ApprenticeName);
         }
 
         [Test]
         public async Task Uln_Is_Mapped()
         {
-            Assert.AreEqual(_source.BulkUploadValidationErrors.First().Uln, _result.BulkUploadValidationErrors.First().Uln);
-            Assert.AreEqual(_source.BulkUploadValidationErrors.Last().Uln, _result.BulkUploadValidationErrors.Last().Uln);
+            Assert.AreEqual(_source.First().Uln, _result.BulkUploadValidationErrors.First().Uln);
+            Assert.AreEqual(_source.Last().Uln, _result.BulkUploadValidationErrors.Last().Uln);
         }
 
         [Test]
         public async Task Error_Text_Are_Mapped()
         {
-            Assert.AreEqual(_source.BulkUploadValidationErrors.First().Errors.First().ErrorText, _result.BulkUploadValidationErrors.First().PropertyErrors.First().ErrorText);
-            Assert.AreEqual(_source.BulkUploadValidationErrors.First().Errors.Last().ErrorText, _result.BulkUploadValidationErrors.First().PropertyErrors.Last().ErrorText);
+            Assert.AreEqual(_source.First().Errors.First().ErrorText, _result.BulkUploadValidationErrors.First().PropertyErrors.First().ErrorText);
+            Assert.AreEqual(_source.First().Errors.Last().ErrorText, _result.BulkUploadValidationErrors.First().PropertyErrors.Last().ErrorText);
 
-            Assert.AreEqual(_source.BulkUploadValidationErrors.Last().Errors.First().ErrorText, _result.BulkUploadValidationErrors.Last().PropertyErrors.First().ErrorText);
-            Assert.AreEqual(_source.BulkUploadValidationErrors.Last().Errors.Last().ErrorText, _result.BulkUploadValidationErrors.Last().PropertyErrors.Last().ErrorText);
+            Assert.AreEqual(_source.Last().Errors.First().ErrorText, _result.BulkUploadValidationErrors.Last().PropertyErrors.First().ErrorText);
+            Assert.AreEqual(_source.Last().Errors.Last().ErrorText, _result.BulkUploadValidationErrors.Last().PropertyErrors.Last().ErrorText);
         }
 
         [Test]
         public async Task Error_Proeprty_Are_Mapped()
         {
-            Assert.AreEqual(_source.BulkUploadValidationErrors.First().Errors.First().Property, _result.BulkUploadValidationErrors.First().PropertyErrors.First().Property);
-            Assert.AreEqual(_source.BulkUploadValidationErrors.First().Errors.Last().Property, _result.BulkUploadValidationErrors.First().PropertyErrors.Last().Property);
+            Assert.AreEqual(_source.First().Errors.First().Property, _result.BulkUploadValidationErrors.First().PropertyErrors.First().Property);
+            Assert.AreEqual(_source.First().Errors.Last().Property, _result.BulkUploadValidationErrors.First().PropertyErrors.Last().Property);
 
-            Assert.AreEqual(_source.BulkUploadValidationErrors.Last().Errors.First().Property, _result.BulkUploadValidationErrors.Last().PropertyErrors.First().Property);
-            Assert.AreEqual(_source.BulkUploadValidationErrors.Last().Errors.Last().Property, _result.BulkUploadValidationErrors.Last().PropertyErrors.Last().Property);
+            Assert.AreEqual(_source.Last().Errors.First().Property, _result.BulkUploadValidationErrors.Last().PropertyErrors.First().Property);
+            Assert.AreEqual(_source.Last().Errors.Last().Property, _result.BulkUploadValidationErrors.Last().PropertyErrors.Last().Property);
         }
     }
 }
