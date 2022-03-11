8369ff5d0fdcb885154e969d097ca0460c44a74a
VasanthaKasirajan3008 <56582101+VasanthaKasirajan3008@users.noreply.github.com>
2022-03-11 14:24:07 +0000

    CON-4388: Coverage for FileUploadReviewApprenticeDetails

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Models/Cohort/FileUploadReviewApprenticeDetailsTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using SFA.DAS.ProviderCommitments.Web.Models.Cohort;
+
+namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Models.Cohort
+{
+    public class FileUploadReviewApprenticeDetailsTests
+    {
+        public FileUploadReviewApprenticeDetails FileUploadReviewApprenticeDetails;
+
+        [SetUp]
+        public void Arrange()
+        {
+            FileUploadReviewApprenticeDetails  = new FileUploadReviewApprenticeDetails();
+        }
+
+        [TestCase(1000, 900, true)]
+        [TestCase(800, 900, false)]
+        [TestCase(800, 800, false)]
+        public void TestExceedsFundingBandCap(int price, int fundingCap, bool fundingCapExceeded)
+        {
+            //Act
+            FileUploadReviewApprenticeDetails.Price = price;
+            FileUploadReviewApprenticeDetails.FundingBandCap = fundingCap;
+
+            //Assert
+            var result = FileUploadReviewApprenticeDetails.ExceedsFundingBandCap;
+            Assert.AreEqual(result, fundingCapExceeded);
+        }
+    }
+}
