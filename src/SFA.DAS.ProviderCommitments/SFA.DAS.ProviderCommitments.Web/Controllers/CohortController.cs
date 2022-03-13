73d68673cf27acaa2fc30358ad5bcfa4b44d09aa
Paul Graham <paul.graham@coprime.co.uk>
2022-03-13 08:04:01 +0000

    added tests

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Controllers/CohortController.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Controllers/CohortController.cs
@@ -195,7 +195,7 @@ namespace SFA.DAS.ProviderCommitments.Web.Controllers
                     {new ErrorDetail("DeliveryModel", "Please select a delivery model option")});
             }
 
-            return View("AddDraftApprenticeship", request);
+            return RedirectToAction("AddDraftApprenticeship", request);
         }
 
 
