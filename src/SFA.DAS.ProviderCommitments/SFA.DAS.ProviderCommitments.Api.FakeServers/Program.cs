e30105538def8947cbf1242552a48f2a18c9fd32
Paul Graham <paul.graham@coprime.co.uk>
2022-03-22 08:52:04 +0000

    Info added to Fake Server Console

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Api.FakeServers/Program.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Api.FakeServers/Program.cs
@@ -14,6 +14,9 @@ namespace SFA.DAS.ProviderCommitments.Api.FakeServers
                 .WithCourseDeliveryModels()
                 .Build();
 
+            Console.WriteLine("PAS Accounts running on port 44378");
+            Console.WriteLine("Approvals Outer API running on port 10234");
+            Console.WriteLine("Course Games Developer (650) has a Single DeliveryModel, all other course have multiple");
             Console.WriteLine("Press any key to stop the APIs server");
             Console.ReadKey();
         }
