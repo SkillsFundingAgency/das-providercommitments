b4a778cf064fb5eba180b9893b8c5fee7c5302a8
Shahzad <hkf-tech@users.noreply.github.com>
2022-03-25 11:19:53 +0000

    CON-4486 model view mapper

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/Cohort/FileUploadValidateViewModelToReviewRequestMapper.cs
@@ -0,0 +1,31 @@
+﻿using SFA.DAS.CommitmentsV2.Shared.Interfaces;
+using SFA.DAS.ProviderCommitments.Interfaces;
+using SFA.DAS.ProviderCommitments.Web.Models.Cohort;
+using System;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ProviderCommitments.Web.Mappers.Cohort
+{
+    public class FileUploadStartViewModelToReviewRequestMapper : IMapper<FileUploadStartViewModel, FileUploadReviewRequest>
+    {
+        private readonly IBulkUploadFileParser _fileParser;
+        private readonly ICacheService _cache;
+        public FileUploadStartViewModelToReviewRequestMapper(IBulkUploadFileParser fileParser, ICacheService cache)
+        {
+            _fileParser = fileParser;
+            _cache = cache;
+        }
+
+        public async Task<FileUploadReviewRequest> Map(FileUploadStartViewModel source)
+        {
+            var csvRecords = _fileParser.GetCsvRecords(source.ProviderId, source.Attachment);
+            var cacheRequestId = await _cache.SetCache(csvRecords);
+
+            return new FileUploadReviewRequest
+            {
+                ProviderId = source.ProviderId,
+                CacheRequestId = cacheRequestId
+            };
+        }
+    }
+}
