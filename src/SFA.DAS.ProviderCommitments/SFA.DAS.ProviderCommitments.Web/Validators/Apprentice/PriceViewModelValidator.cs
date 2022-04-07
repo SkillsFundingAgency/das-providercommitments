49ffa18344d4e5b657db25b1402a5cdcfeb6aed7
Ben Arnold <ben@arnoldfamily.me.uk>
2022-04-07 09:46:27 +0100

    Update error message for employment price during CoP

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Validators/Apprentice/PriceViewModelValidator.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Validators/Apprentice/PriceViewModelValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using SFA.DAS.CommitmentsV2.Shared.Extensions;
+using SFA.DAS.CommitmentsV2.Types;
 using SFA.DAS.ProviderCommitments.Web.Models.Apprentice;
 
 namespace SFA.DAS.ProviderCommitments.Web.Validators.Apprentice
@@ -12,9 +13,25 @@ namespace SFA.DAS.ProviderCommitments.Web.Validators.Apprentice
             RuleFor(x => x.ApprenticeshipHashedId).NotEmpty();
             RuleFor(x => x.EmployerAccountLegalEntityPublicHashedId).NotEmpty();
             RuleFor(x => x.StartDate).Must(field => field.IsValidMonthYear());
-            RuleFor(x => x.Price).NotEmpty().WithMessage("Enter the new agreed apprenticeship price");
-            RuleFor(x => x.Price).GreaterThanOrEqualTo(1).WithMessage("Enter the new agreed apprenticeship price");
-            RuleFor(x => x.Price).LessThanOrEqualTo(100000).WithMessage("The new agreed apprenticeship price must be £100,000 or less");
+
+            When(x => x.DeliveryModel != DeliveryModel.PortableFlexiJob, () =>
+            {
+                RuleFor(x => x.Price).NotEmpty().WithMessage("Enter the new agreed apprenticeship price");
+                RuleFor(x => x.Price).GreaterThanOrEqualTo(1).WithMessage("Enter the new agreed apprenticeship price");
+                RuleFor(x => x.Price).LessThanOrEqualTo(100000).WithMessage("The new agreed apprenticeship price must be £100,000 or less");
+            });
+
+            When(x => x.DeliveryModel == DeliveryModel.PortableFlexiJob, () =>
+            {
+                RuleFor(x => x.Price).NotEmpty().WithMessage("Enter the total agreed apprenticeship price");
+                RuleFor(x => x.Price).GreaterThanOrEqualTo(1).WithMessage("Enter the total agreed apprenticeship price");
+                RuleFor(x => x.Price).LessThanOrEqualTo(100000).WithMessage("The total agreed apprenticeship price must be £100,000 or less");
+                
+                RuleFor(x => x.EmploymentPrice).NotEmpty().WithMessage("Enter the agreed training price for this period of employment");
+                RuleFor(x => x.EmploymentPrice).GreaterThanOrEqualTo(1).WithMessage("The price must be greater than zero");
+                RuleFor(x => x.EmploymentPrice).LessThanOrEqualTo(x => x.Price)
+                    .WithMessage("This price must not be more than than the total agreed apprenticeship price");
+            });
         }
     }
 }
