a3c020af7470db99b967cbee4d5fb71eb1f0ed0c
Ben Arnold <ben@arnoldfamily.me.uk>
2022-04-07 09:54:50 +0100

    Update error message for employment start date during CoP

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Validators/Apprentice/StartDateViewModelValidator.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Validators/Apprentice/StartDateViewModelValidator.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using SFA.DAS.CommitmentsV2.Shared.Interfaces;
 using SFA.DAS.CommitmentsV2.Shared.Models;
+using SFA.DAS.CommitmentsV2.Types;
 using SFA.DAS.ProviderCommitments.Web.Models.Apprentice;
 
 namespace SFA.DAS.ProviderCommitments.Web.Validators.Apprentice
@@ -24,17 +25,9 @@ namespace SFA.DAS.ProviderCommitments.Web.Validators.Apprentice
             RuleFor(x => x.StopDate)
                 .NotEmpty();
             RuleFor(x => x.StartDate)
-                .Must(y => y.HasValue)
-                .WithMessage("Enter the new training start date for this apprenticeship")
-                .When(z => !z.StartDate.HasValue);
-            RuleFor(x => x.StartDate)
                 .Must(y => y.IsValid)
                 .WithMessage("The start date is not valid")
                 .When(z => z.StartDate.HasValue);
-            RuleFor(x => x.StartDate)
-                .Must((y, _) => y.StartDate.Date >= y.StopDate)
-                .WithMessage("The new training start date cannot be before the stop date")
-                .When(a => a.StartDate.HasValue && a.StartDate.IsValid);
             RuleFor(x => x.StartDate)
                 .Must((y, _) => y.StartDate.Date < (new MonthYearModel(y.EndDate).Date))
                 .WithMessage("Enter a start date prior to the new training end date")
@@ -44,6 +37,31 @@ namespace SFA.DAS.ProviderCommitments.Web.Validators.Apprentice
                 .WithMessage("The start date must be no later than one year after the end of the current teaching year")
                 .When(a => a.StartDate.HasValue && a.StartDate.IsValid)
                 .Unless(a => a.EndDate != null && a.StartDate.Date > new MonthYearModel(a.EndDate).Date);
+
+            When(x => x.DeliveryModel != DeliveryModel.PortableFlexiJob, () =>
+            {
+                RuleFor(x => x.StartDate)
+                    .Must(y => y.HasValue)
+                    .WithMessage("Enter the new training start date for this apprenticeship")
+                    .When(z => !z.StartDate.HasValue);
+
+                RuleFor(x => x.StartDate)
+                    .Must((y, _) => y.StartDate.Date >= y.StopDate)
+                    .WithMessage("The new training start date cannot be before the stop date")
+                    .When(a => a.StartDate.HasValue && a.StartDate.IsValid);
+            });
+
+            When(x => x.DeliveryModel == DeliveryModel.PortableFlexiJob, () =>
+            {
+                RuleFor(x => x.StartDate)
+                    .Must(y => y.HasValue)
+                    .WithMessage("You must enter a start date for this employment");
+
+                RuleFor(x => x.StartDate)
+                    .Must((y, _) => y.StartDate.Date >= y.StopDate)
+                    .WithMessage("This date must not be before the previous employment end date")
+                    .When(a => a.StartDate.HasValue && a.StartDate.IsValid);
+            });
         }
     }
 }
