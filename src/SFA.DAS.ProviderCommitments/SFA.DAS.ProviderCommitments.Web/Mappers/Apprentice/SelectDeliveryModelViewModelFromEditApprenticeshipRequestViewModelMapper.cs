69921455b259721a25354d20e0d9f4027fbe1a12
chrisfoster76 <chrisfoster186@googlemail.com>
2022-06-27 11:33:53 +0100

    Post approval changes

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/Apprentice/SelectDeliveryModelViewModelFromEditApprenticeshipRequestViewModelMapper.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/Apprentice/SelectDeliveryModelViewModelFromEditApprenticeshipRequestViewModelMapper.cs
@@ -1,21 +1,26 @@
 ﻿using System.Threading.Tasks;
+using SFA.DAS.CommitmentsV2.Api.Client;
 using SFA.DAS.CommitmentsV2.Shared.Interfaces;
 using SFA.DAS.ProviderCommitments.Web.Models;
 using SFA.DAS.ProviderCommitments.Web.Models.Apprentice.Edit;
 
 namespace SFA.DAS.ProviderCommitments.Web.Mappers.Apprentice
 {
-    public class SelectDeliveryModelViewModelFromEditApprenticeshipRequestViewModelMapper : IMapper<EditApprenticeshipRequestViewModel, SelectDeliveryModelViewModel>    {
+    public class SelectDeliveryModelViewModelFromEditApprenticeshipRequestViewModelMapper : IMapper<EditApprenticeshipRequestViewModel, SelectDeliveryModelViewModel>
+    {
         private readonly ISelectDeliveryModelMapperHelper _helper;
+        private readonly ICommitmentsApiClient _commitmentsApiClient;
 
-        public SelectDeliveryModelViewModelFromEditApprenticeshipRequestViewModelMapper(ISelectDeliveryModelMapperHelper helper)
+        public SelectDeliveryModelViewModelFromEditApprenticeshipRequestViewModelMapper(ISelectDeliveryModelMapperHelper helper, ICommitmentsApiClient commitmentsApiClient)
         {
             _helper = helper;
+            _commitmentsApiClient = commitmentsApiClient;
         }
 
-        public Task<SelectDeliveryModelViewModel> Map(EditApprenticeshipRequestViewModel source)
+        public async Task<SelectDeliveryModelViewModel> Map(EditApprenticeshipRequestViewModel source)
         {
-            return _helper.Map(source.ProviderId, source.CourseCode, 0, source.DeliveryModel);
+            var apprenticeship = await _commitmentsApiClient.GetApprenticeship(source.ApprenticeshipId);
+            return await _helper.Map(source.ProviderId, source.CourseCode, apprenticeship.AccountLegalEntityId, source.DeliveryModel);
         }
     }
 }
