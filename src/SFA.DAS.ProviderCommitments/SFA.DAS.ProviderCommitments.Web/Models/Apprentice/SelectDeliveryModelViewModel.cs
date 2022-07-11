c91c7bb0b5fcd642ed681fb145a172236db3761b
chrisfoster76 <chrisfoster186@googlemail.com>
2022-07-11 08:43:12 +0100

    Refactoring delivery model selection page

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Models/Apprentice/SelectDeliveryModelViewModel.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Models/Apprentice/SelectDeliveryModelViewModel.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using SFA.DAS.ProviderCommitments.Infrastructure.OuterApi.Types;
+using SFA.DAS.ProviderCommitments.Web.Models.Shared;
 
 namespace SFA.DAS.ProviderCommitments.Web.Models.Apprentice
 {
-    public class SelectDeliveryModelViewModel
+    public class SelectDeliveryModelViewModel : IDeliveryModelSelection
     {
         public long ProviderId { get; set; }
         public string ApprenticeshipHashedId { get; set; }
