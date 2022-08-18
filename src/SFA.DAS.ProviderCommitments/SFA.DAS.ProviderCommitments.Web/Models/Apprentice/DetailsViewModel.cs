3affd426c1eb91c9cb531fe0737080880058f667
Alasdair Reid <ali.reid@live.co.uk>
2022-08-18 14:46:58 +0100

    Reworked to use Model.HasMultipleDeliveryModelOptions
    + tests

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Models/Apprentice/DetailsViewModel.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Models/Apprentice/DetailsViewModel.cs
@@ -66,8 +66,7 @@ namespace SFA.DAS.ProviderCommitments.Web.Models.Apprentice
         public int DurationReducedBy { get; set; }
         public string DurationReducedByDisplay => $"{DurationReducedBy} weeks";
         public int PriceReducedBy { get; set; }
-        public string EmployerAccountLegalEntityPublicHashedId { get; set; }
-        public string CourseCode { get; set; }
+        public bool HasMultipleDeliveryModelOptions { get; set; }
 
         public enum DataLockSummaryStatus
         {
