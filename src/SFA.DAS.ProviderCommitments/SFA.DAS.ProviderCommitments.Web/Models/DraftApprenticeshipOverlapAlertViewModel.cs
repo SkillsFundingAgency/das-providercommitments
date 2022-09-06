55db0dac42a6557dc7d62da5c671bcfc41ee6b4b
Corey <corey.faulconbridge@digital.education.gov.uk>
2022-09-06 13:33:20 +0100

    add interface for AuthorizationContextModel

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Models/DraftApprenticeshipOverlapAlertViewModel.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Models/DraftApprenticeshipOverlapAlertViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using SFA.DAS.Authorization.ModelBinding;
 using SFA.DAS.CommitmentsV2.Types;
 using SFA.DAS.ProviderCommitments.Web.RouteValues;
 
 namespace SFA.DAS.ProviderCommitments.Web.Models
 {
-    public class DraftApprenticeshipOverlapAlertViewModel
+    public class DraftApprenticeshipOverlapAlertViewModel : IAuthorizationContextModel
     {
         public long ProviderId { get; set; }
         public string CohortReference { get; set; }
