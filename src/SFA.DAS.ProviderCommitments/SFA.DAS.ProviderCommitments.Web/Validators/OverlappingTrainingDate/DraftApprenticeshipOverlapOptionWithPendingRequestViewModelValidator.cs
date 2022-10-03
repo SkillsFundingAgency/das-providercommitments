79abf6f26283a794029786709b047d870601a7de
Shahzad <shahzad.khan@hkf-technologies.co.uk>
2022-10-03 16:01:01 +0100

    text fix for error

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Validators/OverlappingTrainingDate/DraftApprenticeshipOverlapOptionWithPendingRequestViewModelValidator.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Validators/OverlappingTrainingDate/DraftApprenticeshipOverlapOptionWithPendingRequestViewModelValidator.cs
@@ -8,7 +8,7 @@ namespace SFA.DAS.ProviderCommitments.Web.Validators.OverlappingTrainingDate
         public DraftApprenticeshipOverlapOptionWithPendingRequestViewModelValidator()
         {
             RuleFor(x => x.ProviderId).GreaterThan(0);
-            RuleFor(x => x.OverlapOptions).NotNull().WithMessage("You need to select what you would like to do");
+            RuleFor(x => x.OverlapOptions).NotNull().WithMessage("You need to select whether you want to save the changes");
         }
     }
 }
