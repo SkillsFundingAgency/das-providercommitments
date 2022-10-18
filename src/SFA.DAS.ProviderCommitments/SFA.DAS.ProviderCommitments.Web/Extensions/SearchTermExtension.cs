5340636af17a61eed77a5dd69355bd391983a1e7
chazzysb <cballagan@yahoo.com>
2022-10-18 12:12:31 +0100

    PA-510-IncludeConfirmationFilter

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Extensions/SearchTermExtension.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Extensions/SearchTermExtension.cs
@@ -71,6 +71,11 @@ namespace SFA.DAS.ProviderCommitments.Web.Extensions
                 filters.Add(WebUtility.HtmlEncode(model.SelectedAlert.Value.GetDescription()));
             }
 
+            if (model.SelectedApprenticeConfirmation.HasValue)
+            {
+                filters.Add(WebUtility.HtmlEncode(model.SelectedApprenticeConfirmation.Value.GetDescription()));
+            }
+
             if (model.SelectedDeliveryModel.HasValue)
             {
                 filters.Add(WebUtility.HtmlEncode(model.SelectedDeliveryModel.Value.ToDescription()));
