e809f63e1085cf23905f9e699fd4c0f10bc452fa
Michael Young <mick.young1981@googlemail.com>
2023-01-13 13:56:43 +0000

    Remove validate method

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Controllers/CohortController.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Controllers/CohortController.cs
@@ -200,26 +200,6 @@ namespace SFA.DAS.ProviderCommitments.Web.Controllers
             return View("AddDraftApprenticeship", model);
         }
 
-
-        [HttpPost]
-        [Route("add/validate")]
-        [AjaxValidation]
-        [Authorize(Policy = nameof(PolicyNames.HasContributorOrAbovePermission))]
-        public async Task<IActionResult> Validate(Models.AddDraftApprenticeshipViewModel model)
-        {
-            var request = await _modelMapper.Map<ValidateDraftApprenticeshipApimRequest>(model);
-            request.UserId = User.Upn();
-            try
-            {
-                await _outerApiService.ValidateDraftApprenticeshipForOverlappingTrainingDateRequest(request);
-            }
-            catch (CommitmentsApiModelException ex)
-            {
-                return Json(ex.Errors);
-            }
-            return new OkResult();
-        }
-
         [HttpPost]
         [Route("add/apprenticeship", Name = RouteNames.CohortAddApprenticeship)]
         [DasAuthorize(ProviderOperation.CreateCohort)]
