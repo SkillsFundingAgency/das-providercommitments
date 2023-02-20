9045450ea5a9f908c4ac83f3adefb0fdf0d095af
Alasdair Reid <ali.reid@live.co.uk>
2023-02-20 09:02:57 +0000

    WIP new fields on page

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Models/RecordPriorLearningViewModel.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Models/RecordPriorLearningViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace SFA.DAS.ProviderCommitments.Web.Models
+﻿using System;
+
+namespace SFA.DAS.ProviderCommitments.Web.Models
 {
     public class RecognisePriorLearningRequest : DraftApprenticeshipRequest
     {
@@ -13,6 +15,12 @@
     {
         public int? ReducedPrice { get; set; }
         public int? ReducedDuration { get; set; }
+        public double? DurationReducedByHours { get; set; }
+        public double? WeightageReducedBy { get; set; }
+        public string Qualification { get; set; }
+        public string Reason { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+
     }
 
     public class RecognisePriorLearningResult : DraftApprenticeshipRequest
