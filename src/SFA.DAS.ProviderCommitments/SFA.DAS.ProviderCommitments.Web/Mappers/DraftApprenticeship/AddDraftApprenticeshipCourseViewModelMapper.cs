a248e4433fe4d06786e67a2beea5161ec936118e
chrisfoster76 <chrisfoster186@googlemail.com>
2023-04-18 14:43:55 +0100

    Fixes tests, adds missing test

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/DraftApprenticeship/AddDraftApprenticeshipCourseViewModelMapper.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/DraftApprenticeship/AddDraftApprenticeshipCourseViewModelMapper.cs
@@ -4,7 +4,6 @@ using SFA.DAS.ProviderCommitments.Infrastructure.OuterApi;
 using SFA.DAS.ProviderCommitments.Web.Models;
 using SFA.DAS.ProviderCommitments.Web.Models.DraftApprenticeship;
 using SFA.DAS.ProviderCommitments.Web.Models.Shared;
-using SFA.DAS.ProviderCommitments.Web.Services;
 using System.Threading.Tasks;
 using SFA.DAS.Authorization.Services;
 using SFA.DAS.ProviderCommitments.Infrastructure.OuterApi.Requests.DraftApprenticeship;
@@ -14,23 +13,19 @@ namespace SFA.DAS.ProviderCommitments.Web.Mappers.DraftApprenticeship
 {
     public class AddDraftApprenticeshipCourseViewModelMapper : IMapper<ReservationsAddDraftApprenticeshipRequest, AddDraftApprenticeshipCourseViewModel>
     {
-        private readonly ITempDataStorageService _tempData;
         private readonly IOuterApiClient _apiClient;
         private readonly IAuthorizationService _authorizationService;
 
-        public AddDraftApprenticeshipCourseViewModelMapper(ITempDataStorageService tempData,
+        public AddDraftApprenticeshipCourseViewModelMapper(
             IOuterApiClient apiClient,
             IAuthorizationService authorizationService)
         {
-            _tempData = tempData;
             _apiClient = apiClient;
             _authorizationService = authorizationService;
         }
 
         public async Task<AddDraftApprenticeshipCourseViewModel> Map(ReservationsAddDraftApprenticeshipRequest source)
         {
-            //var draft = _tempData.RetrieveFromCache<AddDraftApprenticeshipViewModel>();
-
             var apiRequest = new GetAddDraftApprenticeshipCourseRequest(source.ProviderId, source.CohortId.Value);
             var apiResponse = await _apiClient.Get<GetAddDraftApprenticeshipCourseResponse>(apiRequest);
 
