43fcc0531cff5009e3a2c37fc29aa1f6085b72f5
Alasdair Reid <ali.reid@live.co.uk>
2023-05-11 14:14:11 +0100

    Reintroduce old mapper

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/Apprentice/RecognisePriorLearningViewModelToResultMapper.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/Apprentice/RecognisePriorLearningViewModelToResultMapper.cs
@@ -1,6 +1,9 @@
 ﻿using SFA.DAS.CommitmentsV2.Api.Client;
 using SFA.DAS.CommitmentsV2.Shared.Interfaces;
+using SFA.DAS.ProviderCommitments.Features;
+using SFA.DAS.ProviderCommitments.Infrastructure.OuterApi;
 using SFA.DAS.ProviderCommitments.Infrastructure.OuterApi.Requests.DraftApprenticeship;
+using SFA.DAS.ProviderCommitments.Infrastructure.OuterApi.Requests.DraftApprenticeships;
 using SFA.DAS.ProviderCommitments.Interfaces;
 using SFA.DAS.ProviderCommitments.Web.Models;
 using System.Threading;
@@ -8,6 +11,46 @@ using System.Threading.Tasks;
 
 namespace SFA.DAS.ProviderCommitments.Web.Mappers.Apprentice
 {
+    public class PriorLearningDetailsViewModelToResultMapper : IMapper<PriorLearningDetailsViewModel, RecognisePriorLearningResult>
+    {
+        private readonly ICommitmentsApiClient _commitmentsApiClient;
+        private readonly IOuterApiClient _outerApiClient;
+        private readonly DAS.Authorization.Services.IAuthorizationService _authorizationService;
+        public PriorLearningDetailsViewModelToResultMapper(ICommitmentsApiClient commitmentsApiClient, DAS.Authorization.Services.IAuthorizationService authorizationService, IOuterApiClient outerApiClient)
+        {
+            _commitmentsApiClient = commitmentsApiClient;
+            _authorizationService = authorizationService;
+            _outerApiClient = outerApiClient;
+        }
+        public async Task<RecognisePriorLearningResult> Map(PriorLearningDetailsViewModel source)
+        {
+            var update = _commitmentsApiClient.PriorLearningDetails(
+                source.CohortId,
+                source.DraftApprenticeshipId,
+                new CommitmentsV2.Api.Types.Requests.PriorLearningDetailsRequest
+                {
+                    DurationReducedBy = source.ReducedDuration,
+                    PriceReducedBy = source.ReducedPrice,
+                    DurationReducedByHours = source.DurationReducedByHours,
+                    WeightageReducedBy = source.WeightageReducedBy,
+                    QualificationsForRplReduction = source.QualificationsForRplReduction,
+                    ReasonForRplReduction = source.ReasonForRplReduction,
+                    Rpl2Mode = await _authorizationService.IsAuthorizedAsync(ProviderFeature.RplExtended)
+                });
+
+            var apprenticeship = _commitmentsApiClient.GetDraftApprenticeship(
+                source.CohortId,
+                source.DraftApprenticeshipId,
+                CancellationToken.None);
+            await Task.WhenAll(update, apprenticeship);
+
+            return new RecognisePriorLearningResult
+            {
+                HasStandardOptions = apprenticeship.Result.HasStandardOptions
+            };
+        }
+    }
+
     public class RecognisePriorLearningViewModelToResultMapper : IMapper<RecognisePriorLearningViewModel, RecognisePriorLearningResult>
     {
         private readonly ICommitmentsApiClient _commitmentsApiClient;
