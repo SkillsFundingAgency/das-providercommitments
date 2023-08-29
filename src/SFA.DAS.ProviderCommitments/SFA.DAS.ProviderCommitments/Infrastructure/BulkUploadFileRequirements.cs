a816b677e734b6858f718f96fad8b37ecf29adef
Paul Graham <paul.graham@coprime.co.uk>
2023-08-29 16:58:38 +0100

    fix bug

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Infrastructure/BulkUploadFileRequirements.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Infrastructure/BulkUploadFileRequirements.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace SFA.DAS.ProviderCommitments.Web.Models.Cohort
 {
@@ -19,13 +20,13 @@ namespace SFA.DAS.ProviderCommitments.Web.Models.Cohort
             nameof(CsvRecord.ProviderRef),
             nameof(CsvRecord.AgreementId),
             nameof(CsvRecord.EmailAddress),
+            nameof(CsvRecord.RecognisePriorLearning),
+            nameof(CsvRecord.DurationReducedBy),
+            nameof(CsvRecord.PriceReducedBy),
         };
 
         public static string[] OptionalHeaders = new[]
         {
-            nameof(CsvRecord.RecognisePriorLearning),
-            nameof(CsvRecord.DurationReducedBy),
-            nameof(CsvRecord.PriceReducedBy),
             nameof(CsvRecord.TrainingTotalHours),
             nameof(CsvRecord.IsDurationReducedByRPL),
             nameof(CsvRecord.TrainingHoursReduction)
@@ -40,11 +41,31 @@ namespace SFA.DAS.ProviderCommitments.Web.Models.Cohort
             return headers.Length >= MinimumColumnCount && headers.Length <= MaximumColumnCount;
         }
 
+        public static bool HasMinimumRequiredColumns(string[] headers)
+        {
+            return headers.Contains(nameof(CsvRecord.CohortRef), StringComparer.InvariantCultureIgnoreCase) &&
+                    headers.Contains(nameof(CsvRecord.ULN), StringComparer.InvariantCultureIgnoreCase) &&
+                    headers.Contains(nameof(CsvRecord.FamilyName), StringComparer.InvariantCultureIgnoreCase) &&
+                    headers.Contains(nameof(CsvRecord.GivenNames), StringComparer.InvariantCultureIgnoreCase) &&
+                    headers.Contains(nameof(CsvRecord.DateOfBirth), StringComparer.InvariantCultureIgnoreCase) &&
+                    headers.Contains(nameof(CsvRecord.StdCode), StringComparer.InvariantCultureIgnoreCase) &&
+                    headers.Contains(nameof(CsvRecord.StartDate), StringComparer.InvariantCultureIgnoreCase) &&
+                    headers.Contains(nameof(CsvRecord.EndDate), StringComparer.InvariantCultureIgnoreCase) &&
+                    headers.Contains(nameof(CsvRecord.TotalPrice), StringComparer.InvariantCultureIgnoreCase) &&
+                    headers.Contains(nameof(CsvRecord.EPAOrgID), StringComparer.InvariantCultureIgnoreCase) &&
+                    headers.Contains(nameof(CsvRecord.ProviderRef), StringComparer.InvariantCultureIgnoreCase) &&
+                    headers.Contains(nameof(CsvRecord.AgreementId), StringComparer.InvariantCultureIgnoreCase) &&
+                    headers.Contains(nameof(CsvRecord.EmailAddress), StringComparer.InvariantCultureIgnoreCase) &&
+                    headers.Contains(nameof(CsvRecord.RecognisePriorLearning), StringComparer.InvariantCultureIgnoreCase) &&
+                    headers.Contains(nameof(CsvRecord.DurationReducedBy), StringComparer.InvariantCultureIgnoreCase) &&
+                    headers.Contains(nameof(CsvRecord.PriceReducedBy), StringComparer.InvariantCultureIgnoreCase);
+        }
+
         public static bool IsRplExtendedUpload(string[] headers)
         {
-            return (headers.Contains(nameof(CsvRecord.TrainingTotalHours)) &&
-                    headers.Contains(nameof(CsvRecord.IsDurationReducedByRPL)) &&
-                    headers.Contains(nameof(CsvRecord.TrainingHoursReduction)));
+            return headers.Contains(nameof(CsvRecord.TrainingTotalHours), StringComparer.InvariantCultureIgnoreCase) &&
+                    headers.Contains(nameof(CsvRecord.IsDurationReducedByRPL), StringComparer.InvariantCultureIgnoreCase) &&
+                    headers.Contains(nameof(CsvRecord.TrainingHoursReduction), StringComparer.InvariantCultureIgnoreCase);
         }
     }
 }
