a2caaf1f4eb63b87509fe7b9b4f77dd66f1e6dd9
Jon Harris <jon.harris@digital.education.gov.uk>
2023-09-28 13:41:48 +0100

    minor refactor

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Validators/FileUpload/FileUploadValidationHelper.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Validators/FileUpload/FileUploadValidationHelper.cs
@@ -12,7 +12,7 @@ namespace SFA.DAS.ProviderCommitments.Web.Validators.FileUpload
 {
     public class FileUploadValidationHelper
     {
-        private BulkUploadFileValidationConfiguration _csvConfiguration;
+        private readonly BulkUploadFileValidationConfiguration _csvConfiguration;
         private const int EXTENDEDRPLCOLUMNCOUNT = 19;
 
         public FileUploadValidationHelper(BulkUploadFileValidationConfiguration config)
@@ -50,33 +50,33 @@ namespace SFA.DAS.ProviderCommitments.Web.Validators.FileUpload
         private bool CheckFileSize(IFormFile file)
         {
             var maxFileSize = _csvConfiguration.MaxBulkUploadFileSize * 1024; // Bytes
-            return (file.Length <= maxFileSize);
+            return file.Length <= maxFileSize;
         }
 
-        private bool CheckFileType(IFormFile file)
+        private static bool CheckFileType(IFormFile file)
         {
             return file.FileName.ToLower().EndsWith(".csv");
         }
 
-        private async Task<bool> CheckEmptyFileContent(IFormFile file, CancellationToken cancellation)
+        private static async Task<bool> CheckEmptyFileContent(IFormFile file, CancellationToken cancellation)
         {
             var fileData = await ReadFileAsync(file);
             return fileData.rowCount > 0;
         }
 
-        private async Task<bool> CheckApprenticeContent(IFormFile file, CancellationToken cancellation)
+        private static async Task<bool> CheckApprenticeContent(IFormFile file, CancellationToken cancellation)
         {
             var fileData = await ReadFileAsync(file);
             return fileData.rowCount > 1;
         }
 
-        private async Task<bool> CheckFileColumnCount(IFormFile file, CancellationToken cancellation)
+        private static async Task<bool> CheckFileColumnCount(IFormFile file, CancellationToken cancellation)
         {
-            var fileContent = new StreamReader(file.OpenReadStream()).ReadToEnd();
+            var fileContent = await new StreamReader(file.OpenReadStream()).ReadToEndAsync();
             using var reader = new StringReader(fileContent);
 
             var firstLine = await reader.ReadLineAsync();
-            var firstlineData = (firstLine).Split(',');
+            var firstlineData = firstLine.Split(',');
 
             return
                 BulkUploadFileRequirements.CheckHeaderCount(firstlineData)
@@ -88,7 +88,7 @@ namespace SFA.DAS.ProviderCommitments.Web.Validators.FileUpload
 
                 while ((line = await reader.ReadLineAsync()) != null)
                 {
-                    if (line.Split(',').Count() != count)
+                    if (line.Split(',').Length != count)
                     {
                         return false;
                     }
@@ -104,20 +104,20 @@ namespace SFA.DAS.ProviderCommitments.Web.Validators.FileUpload
             return fileData.rowCount <= _csvConfiguration.MaxAllowedFileRowCount;
         }
 
-        private async Task<bool> CheckColumnHeaders(IFormFile file, CancellationToken cancellation)
+        private static async Task<bool> CheckColumnHeaders(IFormFile file, CancellationToken cancellation)
         {
-            var fileContent = new StreamReader(file.OpenReadStream()).ReadToEnd();
+            var fileContent = await new StreamReader(file.OpenReadStream()).ReadToEndAsync();
             using var reader = new StringReader(fileContent);
 
             var firstLine = await reader.ReadLineAsync();
-            var firstlineData = (firstLine).Split(',');
+            var firstlineData = firstLine.Split(',');
 
             if (!BulkUploadFileRequirements.HasMinimumRequiredColumns(firstlineData))
             {
                 return false;
             }
 
-            if (firstlineData.Count() == EXTENDEDRPLCOLUMNCOUNT)
+            if (firstlineData.Length == EXTENDEDRPLCOLUMNCOUNT)
             {
                 if (!BulkUploadFileRequirements.IsRplExtendedUpload(firstlineData))
                 {
@@ -127,39 +127,40 @@ namespace SFA.DAS.ProviderCommitments.Web.Validators.FileUpload
             return true;
         }
 
-        private async Task<(string[] firstlineData, int rowCount)> ReadFileAsync(IFormFile file)
+        private static async Task<(string[] firstlineData, int rowCount)> ReadFileAsync(IFormFile file)
         {
             var firstLineData = Array.Empty<string>();
 
-            var fileContent = new StreamReader(file.OpenReadStream()).ReadToEnd();
-            using (var reader = new StringReader(fileContent))
+            var fileContent = await new StreamReader(file.OpenReadStream()).ReadToEndAsync();
+            using var reader = new StringReader(fileContent);
+            
+            var lineCounter = 0;
+
+            while (reader.Peek() >= 0)
             {
-                int lineCounter = 0;
+                var lineContent = await reader.ReadLineAsync();
 
-                while (reader.Peek() >= 0)
+                if (string.IsNullOrWhiteSpace(lineContent))
                 {
-                    string lineContent = await reader.ReadLineAsync();
-                    if (!string.IsNullOrWhiteSpace(lineContent))
+                    continue;
+                }
+                
+                if (firstLineData.Length == 0)
+                {
+                    firstLineData = lineContent.Split(',');
+                    lineCounter++;
+                }
+                else
+                {
+                    var lineContents = lineContent.Split(',');
+                    if (!IsEmptyRow(lineContents))
                     {
-                        if (firstLineData.Length == 0)
-                        {
-                            firstLineData = lineContent.Split(',');
-                            lineCounter++;
-
-                        }
-                        else
-                        {
-                            var lineContents = lineContent.Split(',');
-                            if (!IsEmptyRow(lineContents))
-                            {
-                                lineCounter++;
-                            }
-                        }
+                        lineCounter++;
                     }
                 }
-
-                return (firstLineData, lineCounter);
             }
+
+            return (firstLineData, lineCounter);
         }
 
         private static bool IsEmptyRow(string[] lineContents)
