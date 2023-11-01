2f880a9e77878126770e87b83db74c7b23c2dbb1
Tom Gough <tom.gough@talentconsulting.uk>
2023-11-01 12:00:30 +0000

    Mapping for price episode fields correctly for both inner and outer api responses

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Extensions/InnerApiPriceEpisodeExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SFA.DAS.CommitmentsV2.Api.Types.Responses;
+
+namespace SFA.DAS.ProviderCommitments.Web.Extensions
+{
+    public static class InnerApiPriceEpisodeExtensions
+    {
+        public static decimal GetCost(this IEnumerable<GetPriceEpisodesResponse.PriceEpisode> priceEpisodes)
+        {
+            return priceEpisodes.GetCost(DateTime.UtcNow);
+        }
+
+        public static decimal GetCost(this IEnumerable<GetPriceEpisodesResponse.PriceEpisode> priceEpisodes,
+            DateTime effectiveDate)
+        {
+            return GetEffectivePriceEpisode(priceEpisodes, effectiveDate).Cost;
+        }
+
+        private static GetPriceEpisodesResponse.PriceEpisode GetEffectivePriceEpisode(
+            IEnumerable<GetPriceEpisodesResponse.PriceEpisode> priceEpisodes, DateTime effectiveDate)
+        {
+            var episodes = priceEpisodes.ToList();
+
+            var episode = episodes.FirstOrDefault(x =>
+                x.FromDate <= effectiveDate && (x.ToDate == null || x.ToDate >= effectiveDate));
+
+            return episode ?? episodes.First();
+        }
+    }
+}
