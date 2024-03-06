98176d52692b9fa169763869d02775ccd5eb7740
Jon Harris <jon.harris@digital.education.gov.uk>
2024-03-06 15:49:38 +0000

    Refactor to remove FluentTests from test class.

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Authorization/FeatureToggles/AuthorizationHandlerTests.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Authorization/FeatureToggles/AuthorizationHandlerTests.cs
@@ -4,108 +4,139 @@ using System.Linq;
 using FluentAssertions;
 using SFA.DAS.ProviderCommitments.Authorization;
 using SFA.DAS.ProviderCommitments.Interfaces;
-using SFA.DAS.ProviderCommitments.Web.Authorization;
 using SFA.DAS.ProviderCommitments.Web.Authorization.Context;
 using SFA.DAS.ProviderCommitments.Web.Authorization.Errors;
 using SFA.DAS.ProviderCommitments.Web.Authorization.FeatureToggles;
 using SFA.DAS.ProviderCommitments.Web.Authorization.Handlers;
 using SFA.DAS.ProviderCommitments.Web.Authorization.Models;
-using SFA.DAS.Testing;
 
-namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Authorization.FeatureToggles
+namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Authorization.FeatureToggles;
+
+[TestFixture]
+[Parallelizable]
+public class AuthorizationHandlerTests 
 {
-    [TestFixture]
-    [Parallelizable]
-    public class AuthorizationHandlerTests : FluentTest<EmployerFeaturesAuthorizationHandlerTestsFixture>
+    private EmployerFeaturesAuthorizationHandlerTestsFixture _fixture;
+
+    [SetUp]
+    public void Setup() => _fixture = new EmployerFeaturesAuthorizationHandlerTestsFixture();
+
+    [Test]
+    public async Task GetAuthorizationResult_WhenOptionsAreNotAvailable_ThenShouldReturnValidAuthorizationResult()
     {
-        [Test]
-        public Task GetAuthorizationResult_WhenOptionsAreNotAvailable_ThenShouldReturnValidAuthorizationResult()
-        {
-            return TestAsync(f => f.GetAuthorizationResult(), (f, r) => r.Should().NotBeNull().And.Match<AuthorizationResult>(r2 => r2.IsAuthorized));
-        }
+        var result = await _fixture.GetAuthorizationResult();
         
-        [Test]
-        public Task GetAuthorizationResult_WhenAndedOptionsAreAvailable_ThenShouldThrowNotImplementedException()
-        {
-            return TestExceptionAsync(f => f.SetAndedOptions(), f => f.GetAuthorizationResult(), (f, r) => r.Should().ThrowAsync<NotImplementedException>());
-        }
-        
-        [Test]
-        public Task GetAuthorizationResult_WhenOredOptionIsAvailable_ThenShouldThrowNotImplementedException()
-        {
-            return TestExceptionAsync(f => f.SetOredOption(), f => f.GetAuthorizationResult(), (f, r) => r.Should().ThrowAsync<NotImplementedException>());
-        }
-
-        [Test]
-        public Task GetAuthorizationResult_WhenOptionsAreAvailableAndFeatureIsEnabled_ThenShouldReturnAuthorizedAuthorizationResult()
-        {
-            return TestAsync(f => f.SetOption().SetFeatureToggle(true), f => f.GetAuthorizationResult(), (f, r) => r.Should().NotBeNull()
-                .And.Match<AuthorizationResult>(r2 => r2.IsAuthorized));
-        }
-
-        [Test]
-        public Task GetAuthorizationResult_WhenOptionsAreAvailableAndFeatureIsNotEnabled_ThenShouldReturnUnauthorizedAuthorizationResult()
-        {
-            return TestAsync(f => f.SetOption().SetFeatureToggle(false), f => f.GetAuthorizationResult(), (f, r) => r.Should().NotBeNull()
-                .And.Match<AuthorizationResult>(r2 => !r2.IsAuthorized && r2.Errors.Count() == 1 && r2.HasError<ProviderFeatureNotEnabled>()));
-        }
+        result.Should()
+            .NotBeNull()
+            .And
+            .Match<AuthorizationResult>(r2 => r2.IsAuthorized);
     }
 
-    public class EmployerFeaturesAuthorizationHandlerTestsFixture
+    [Test]
+    public void GetAuthorizationResult_WhenAndedOptionsAreAvailable_ThenShouldThrowNotImplementedException()
     {
-        public List<string> Options { get; set; }
-        public IAuthorizationContext AuthorizationContext { get; set; }
-        public IAuthorizationHandler Handler { get; set; }
-        public Mock<IFeatureTogglesService<FeatureToggle>> FeatureTogglesService { get; set; }
+        _fixture.SetAndedOptions();
         
-        public EmployerFeaturesAuthorizationHandlerTestsFixture()
-        {
-            Options = new List<string>();
-            AuthorizationContext = new AuthorizationContext();
-            FeatureTogglesService = new Mock<IFeatureTogglesService<FeatureToggle>>();
-            Handler = new ProviderFeaturesAuthorizationHandler(FeatureTogglesService.Object);
-        }
+        var result = async () => await _fixture.GetAuthorizationResult();
+        
+        result.Should().ThrowAsync<NotImplementedException>();
+    }
 
-        public Task<AuthorizationResult> GetAuthorizationResult()
-        {
-            return Handler.GetAuthorizationResult(Options, AuthorizationContext);
-        }
+    [Test]
+    public void GetAuthorizationResult_WhenOredOptionIsAvailable_ThenShouldThrowNotImplementedException()
+    {
+        _fixture.SetOredOption();
+        
+        var result = async () => await _fixture.GetAuthorizationResult();
+        
+        result.Should().ThrowAsync<NotImplementedException>();
+    }
 
-        public EmployerFeaturesAuthorizationHandlerTestsFixture SetNonFeatureOptions()
-        {
-            Options.AddRange(new [] { "Foo", "Bar" });
+    [Test]
+    public async Task GetAuthorizationResult_WhenOptionsAreAvailableAndFeatureIsEnabled_ThenShouldReturnAuthorizedAuthorizationResult()
+    {
+        _fixture
+            .SetOption()
+            .SetFeatureToggle(true);
 
-            return this;
-        }
+        var result = await _fixture.GetAuthorizationResult();
+        
+        result.Should()
+            .NotBeNull()
+            .And
+            .Match<AuthorizationResult>(r2 => r2.IsAuthorized);
+    }
 
-        public EmployerFeaturesAuthorizationHandlerTestsFixture SetAndedOptions()
-        {
-            Options.AddRange(new [] { "ProviderRelationships", "Tickles" });
-            
-            return this;
-        }
+    [Test]
+    public async Task GetAuthorizationResult_WhenOptionsAreAvailableAndFeatureIsNotEnabled_ThenShouldReturnUnauthorizedAuthorizationResult()
+    {
+        _fixture
+            .SetOption()
+            .SetFeatureToggle(false);
 
-        public EmployerFeaturesAuthorizationHandlerTestsFixture SetOredOption()
-        {
-            Options.Add($"ProviderRelationships,ProviderRelationships");
-            
-            return this;
-        }
+        var result = await _fixture.GetAuthorizationResult();
 
-        public EmployerFeaturesAuthorizationHandlerTestsFixture SetOption()
-        {
-            Options.AddRange(new [] { "ProviderRelationships" });
+        result.Should()
+            .NotBeNull()
+            .And
+            .Match<AuthorizationResult>(r2 => !r2.IsAuthorized && r2.Errors.Count() == 1 && r2.HasError<ProviderFeatureNotEnabled>());
+    }
+}
 
-            return this;
-        }
+public class EmployerFeaturesAuthorizationHandlerTestsFixture
+{
+    public List<string> Options { get; set; }
+    public IAuthorizationContext AuthorizationContext { get; set; }
+    public IAuthorizationHandler Handler { get; set; }
+    public Mock<IFeatureTogglesService<FeatureToggle>> FeatureTogglesService { get; set; }
 
-        public EmployerFeaturesAuthorizationHandlerTestsFixture SetFeatureToggle(bool isEnabled, bool? isAccountIdWhitelisted = null, bool? isUserEmailWhitelisted = null)
-        {
-            var option = Options.Single();
+    public EmployerFeaturesAuthorizationHandlerTestsFixture()
+    {
+        Options = new List<string>();
+        AuthorizationContext = new AuthorizationContext();
+        FeatureTogglesService = new Mock<IFeatureTogglesService<FeatureToggle>>();
+        Handler = new ProviderFeaturesAuthorizationHandler(FeatureTogglesService.Object);
+    }
 
-            FeatureTogglesService.Setup(s => s.GetFeatureToggle(option)).Returns(new FeatureToggle { Feature = "ProviderRelationships", IsEnabled = isEnabled });
-            
-            return this;
-        }
+    public Task<AuthorizationResult> GetAuthorizationResult()
+    {
+        return Handler.GetAuthorizationResult(Options, AuthorizationContext);
+    }
+
+    public EmployerFeaturesAuthorizationHandlerTestsFixture SetNonFeatureOptions()
+    {
+        Options.AddRange(new[] { "Foo", "Bar" });
+
+        return this;
+    }
+
+    public EmployerFeaturesAuthorizationHandlerTestsFixture SetAndedOptions()
+    {
+        Options.AddRange(new[] { "ProviderRelationships", "Tickles" });
+
+        return this;
+    }
+
+    public EmployerFeaturesAuthorizationHandlerTestsFixture SetOredOption()
+    {
+        Options.Add($"ProviderRelationships,ProviderRelationships");
+
+        return this;
+    }
+
+    public EmployerFeaturesAuthorizationHandlerTestsFixture SetOption()
+    {
+        Options.AddRange(new[] { "ProviderRelationships" });
+
+        return this;
+    }
+
+    public EmployerFeaturesAuthorizationHandlerTestsFixture SetFeatureToggle(bool isEnabled, bool? isAccountIdWhitelisted = null, bool? isUserEmailWhitelisted = null)
+    {
+        var option = Options.Single();
+
+        FeatureTogglesService.Setup(s => s.GetFeatureToggle(option)).Returns(new FeatureToggle { Feature = "ProviderRelationships", IsEnabled = isEnabled });
+
+        return this;
     }
 }
