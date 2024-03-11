9699d121dfb72fd8adfcd57d74afc848f4209993
Jon Harris <jon.harris@digital.education.gov.uk>
2024-03-11 09:07:45 +0000

    OuterApi URI fix

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Infrastructure/OuterApi/Requests/Authorization/GetCohortAccessRequest.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Infrastructure/OuterApi/Requests/Authorization/GetCohortAccessRequest.cs
@@ -5,7 +5,7 @@ namespace SFA.DAS.ProviderCommitments.Infrastructure.OuterApi.Requests.Authoriza
 
 public record GetCohortAccessRequest(Party Party, long PartyId, long CohortId): IGetApiRequest
 {
-    public string GetUrl => "approvals/authorization/access-cohort";
+    public string GetUrl => "authorization/access-cohort";
 }
 
 public record GetCohortAccessResponse
