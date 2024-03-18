5626da7c8f82d4c656cfbd6e5010b0ff6bcae525
Jon Harris <jon.harris@digital.education.gov.uk>
2024-03-18 12:33:35 +0000

    Logging

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Authentication/AuthenticationService.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Authentication/AuthenticationService.cs
@@ -1,30 +1,31 @@
 using System.Security.Claims;
+using Newtonsoft.Json;
 using SFA.DAS.CommitmentsV2.Types;
 using SFA.DAS.ProviderCommitments.Infrastructure.OuterApi;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace SFA.DAS.ProviderCommitments.Web.Authentication;
 
-public class AuthenticationService : IAuthenticationService, IAuthenticationServiceForApim
+public class AuthenticationService(IHttpContextAccessor httpContextAccessor, ILogger<AuthenticationService> logger) : IAuthenticationService, IAuthenticationServiceForApim
 {
-    private readonly IHttpContextAccessor _httpContextAccessor;
-
-    public AuthenticationService(IHttpContextAccessor httpContextAccessor)
-    {
-        _httpContextAccessor = httpContextAccessor;
-    }
-
     public string UserId => GetUserClaimAsString(ProviderClaims.Upn) ?? GetUserClaimAsString("sub");
     public string UserName => GetUserClaimAsString(ProviderClaims.Name) ?? GetUserClaimAsString(ProviderClaims.DisplayName);
     public string UserEmail => GetUserClaimAsString(ProviderClaims.Email) ?? GetUserClaimAsString("email");
 
     public bool IsUserAuthenticated()
     {
-        return _httpContextAccessor.HttpContext.User.Identity.IsAuthenticated;
+        logger.LogWarning("AuthenticationService.IsUserAuthenticated(). HttpContextAccessor null?: {Check1}. HttpContext null?: {Check2}. User: {User}", 
+            httpContextAccessor == null,
+            httpContextAccessor.HttpContext == null,
+            JsonSerializer.Serialize(httpContextAccessor?.HttpContext?.User)
+            );
+        
+        return httpContextAccessor.HttpContext.User.Identity.IsAuthenticated;
     }
 
     public bool TryGetUserClaimValue(string key, out string value)
     {
-        var claimsIdentity = (ClaimsIdentity)_httpContextAccessor.HttpContext.User.Identity;
+        var claimsIdentity = (ClaimsIdentity)httpContextAccessor.HttpContext.User.Identity;
         var claim = claimsIdentity.FindFirst(key);
         var exists = claim != null;
 
@@ -35,7 +36,7 @@ public class AuthenticationService : IAuthenticationService, IAuthenticationServ
         
     public bool TryGetUserClaimValues(string key, out IEnumerable<string> values)
     {
-        var claimsIdentity = (ClaimsIdentity)_httpContextAccessor.HttpContext.User.Identity;
+        var claimsIdentity = (ClaimsIdentity)httpContextAccessor.HttpContext.User.Identity;
         var claims = claimsIdentity.FindAll(key);
 
         values = claims.Select(c => c.Value).ToList();
