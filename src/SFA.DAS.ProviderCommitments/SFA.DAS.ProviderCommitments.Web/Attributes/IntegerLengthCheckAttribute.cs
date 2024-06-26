9d7931a197b14556d82d1b34b2569291a8cb91d5
ogrwill <will.ogrady@digital.education.gov.uk>
2024-06-26 14:03:30 +0100

    adding new length check attribute to validate Cost field

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Attributes/IntegerLengthCheckAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace SFA.DAS.ProviderCommitments.Web.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class IntegerLengthCheckAttribute : Attribute
+    {
+        public string PropertyName { get; set; }
+        public string DisplayName { get; set; }
+        public int MaxNumberOfDigits { get; set; }
+        public string CustomLengthErrorMessage { get; set; }
+
+        public IntegerLengthCheckAttribute(string propertyName, string displayName, int maxNumberOfDigits)
+        {
+            PropertyName = propertyName;
+            DisplayName = displayName;
+            CustomLengthErrorMessage = $"{displayName} must be {maxNumberOfDigits} numbers or fewer";
+            MaxNumberOfDigits = maxNumberOfDigits;
+        }
+    }
+}
---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/ModelBinding/IntegerLengthCheckModelBinder.cs
@@ -0,0 +1,93 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using SFA.DAS.ProviderCommitments.Web.Attributes;
+
+namespace SFA.DAS.ProviderCommitments.Web.ModelBinding
+{
+    public class IntegerLengthCheckModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var valueResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+            bindingContext.ModelState.SetModelValue(bindingContext.ModelName, valueResult);
+            var converter = TypeDescriptor.GetConverter(bindingContext.ModelType);
+
+            var integerLengthCheckAttribute = (bindingContext.ModelMetadata as DefaultModelMetadata)?
+                   .Attributes.PropertyAttributes
+                   .OfType<IntegerLengthCheckAttribute>()
+                   .FirstOrDefault();
+
+            try
+            {
+                // throws if a decimal
+                var typeCheck = converter.ConvertFrom(valueResult.FirstValue);
+
+                // only check length if it is an integer
+                var result = TryParseNullableInt(valueResult.FirstValue);
+                if (result.HasValue)
+                {
+                    if (!HasMoreDigitsThan(result.Value, integerLengthCheckAttribute?.MaxNumberOfDigits ?? int.MaxValue))
+                    {
+                        bindingContext.Result = ModelBindingResult.Success(result);
+                    }
+                    else
+                    {
+                        SetFailureMessageForLength(bindingContext, integerLengthCheckAttribute);
+                    }
+                }
+                else
+                {
+                    bindingContext.Result = ModelBindingResult.Success(result);
+                }
+            }
+            catch (ArgumentException)
+            {
+                SetFailureMessageIfNotInteger(bindingContext, integerLengthCheckAttribute, valueResult.FirstValue);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private static void SetFailureMessageForLength(ModelBindingContext bindingContext,
+           IntegerLengthCheckAttribute integerLengthCheckAttribute)
+        {
+            bindingContext.Result = ModelBindingResult.Failed();
+            bindingContext.ModelState.TryAddModelError(
+                integerLengthCheckAttribute.PropertyName,
+                integerLengthCheckAttribute.CustomLengthErrorMessage);
+        }
+
+        private static void SetFailureMessageIfNotInteger(ModelBindingContext bindingContext,
+            IntegerLengthCheckAttribute integerLengthCheckAttribute,
+            string value)
+        {
+            bindingContext.Result = ModelBindingResult.Failed();
+            bindingContext.ModelState.TryAddModelError(
+              integerLengthCheckAttribute.PropertyName,
+              $"The value '{value}' is not valid for {integerLengthCheckAttribute.DisplayName}.");
+        }
+
+        private static bool HasMoreDigitsThan(int? number, int maxDigits)
+        {
+            if ( number == null )
+            {
+                return false;
+            }
+            
+            number = Math.Abs(number.Value);
+            int numDigits = number.ToString().Length;
+            return numDigits > maxDigits;
+        }
+
+        public static int? TryParseNullableInt(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return null;
+            }
+
+            return int.TryParse(input, out int number) ? number : null;
+        }
+    }
+}
