68e8d52cff3d70954aa7ef873561280e361233fd
ogrwill <will.ogrady@digital.education.gov.uk>
2024-08-21 12:53:53 +0100

    update / add unit tests

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Infrastructure/OuterApi/Requests/PostApprenticeshipsCSVRequest.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Infrastructure/OuterApi/Requests/PostApprenticeshipsCSVRequest.cs
@@ -9,7 +9,6 @@ namespace SFA.DAS.ProviderCommitments.Infrastructure.OuterApi.Requests
     {
         public long? ProviderId { get; set; }
 
-        public string FilterQuery { get; set; }
         public string PostUrl => $"provider/{ProviderId}/apprenticeships/download";
 
         public object Data { get; set; }
@@ -18,7 +17,6 @@ namespace SFA.DAS.ProviderCommitments.Infrastructure.OuterApi.Requests
         {
             ProviderId = providerId;
             Data = body;
-            FilterQuery = CreateFilterQuery(body);
         }
 
         public class Body
@@ -40,40 +38,6 @@ namespace SFA.DAS.ProviderCommitments.Infrastructure.OuterApi.Requests
             public ConfirmationStatus? ApprenticeConfirmationStatus { get; set; }
 
             public DeliveryModel? DeliveryModel { get; set; }
-        }
-     
-        private static string CreateFilterQuery(Body request)
-        {
-            var queryParameters = new List<string>();
-
-            if (!string.IsNullOrEmpty(request.SearchTerm))
-                queryParameters.Add($"searchTerm={WebUtility.UrlEncode(request.SearchTerm)}");
-
-            if (!string.IsNullOrEmpty(request.EmployerName))
-                queryParameters.Add($"employerName={WebUtility.UrlEncode(request.EmployerName)}");
-
-            if (!string.IsNullOrEmpty(request.CourseName))
-                queryParameters.Add($"courseName={WebUtility.UrlEncode(request.CourseName)}");
-
-            if (request.Status.HasValue)
-                queryParameters.Add($"status={WebUtility.UrlEncode(request.Status.Value.ToString())}");
-
-            if (request.StartDate.HasValue)
-                queryParameters.Add($"startDate={WebUtility.UrlEncode(request.StartDate.Value.ToString("u"))}");
-
-            if (request.EndDate.HasValue)
-                queryParameters.Add($"endDate={WebUtility.UrlEncode(request.EndDate.Value.ToString("u"))}");
-
-            if (request.Alert.HasValue)
-                queryParameters.Add($"alert={WebUtility.UrlEncode(request.Alert.Value.ToString())}");
-
-            if (request.ApprenticeConfirmationStatus.HasValue)
-                queryParameters.Add($"apprenticeConfirmationStatus={WebUtility.UrlEncode(request.ApprenticeConfirmationStatus.ToString())}");
-
-            if (request.DeliveryModel.HasValue)
-                queryParameters.Add($"deliveryModel={WebUtility.UrlEncode(request.DeliveryModel.ToString())}");
-
-            return queryParameters.Any() ? "&" + string.Join("&", queryParameters) : string.Empty;
-        }
+        }        
     }
 }
