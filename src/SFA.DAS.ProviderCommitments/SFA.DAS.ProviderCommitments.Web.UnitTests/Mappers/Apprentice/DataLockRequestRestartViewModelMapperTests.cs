be41b753f25ebabddb6401cedcf08ce61b3b2bc8
Jon Harris <jon.harris@fuitsu.com>
2024-09-09 12:34:13 +0100

    Refactor to use FluentAssertions

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Mappers/Apprentice/DataLockRequestRestartViewModelMapperTests.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Mappers/Apprentice/DataLockRequestRestartViewModelMapperTests.cs
@@ -7,212 +7,211 @@ using SFA.DAS.CommitmentsV2.Types;
 using SFA.DAS.ProviderCommitments.Web.Mappers.Apprentice;
 using SFA.DAS.ProviderCommitments.Web.Models.Apprentice;
 
-namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Mappers.Apprentice
+namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Mappers.Apprentice;
+
+[TestFixture]
+public class DataLockRequestRestartViewModelMapperTests
 {
-    [TestFixture]
-    public class DataLockRequestRestartViewModelMapperTests
+    private Mock<ICommitmentsApiClient> _mockCommitmentsApiClient;        
+    private DataLockRequestRestartRequest _dataLockRequestRestartRequest;        
+    private DataLockRequestRestartRequestToViewModelMapper _mapper;
+    private GetDataLockSummariesResponse _getDataLockSummariesResponse;
+    private GetApprenticeshipResponse _getApprenticeshipResponse;
+    private GetAllTrainingProgrammesResponse _getAllTrainingProgrammesResponse;
+    private List<DataLock> _dataLocksWithCourseMismatch;
+    private List<TrainingProgramme> _trainingProgrammes;
+
+    [SetUp]
+    public void Arrange()
     {
-        private Mock<ICommitmentsApiClient> _mockCommitmentsApiClient;        
-        private DataLockRequestRestartRequest _dataLockRequestRestartRequest;        
-        private DataLockRequestRestartRequestToViewModelMapper _mapper;
-        private GetDataLockSummariesResponse _getDataLockSummariesResponse;
-        private GetApprenticeshipResponse _getApprenticeshipResponse;
-        private GetAllTrainingProgrammesResponse _getAllTrainingProgrammesResponse;
-        private List<DataLock> _dataLocksWithCourseMismatch;
-        private List<TrainingProgramme> _trainingProgrammes;
+        var autoFixture = new Fixture();
 
-        [SetUp]
-        public void Arrange()
+        _dataLockRequestRestartRequest = autoFixture.Create<DataLockRequestRestartRequest>();
+
+        _dataLocksWithCourseMismatch = new List<DataLock>
         {
-            var autoFixture = new Fixture();
-
-            _dataLockRequestRestartRequest = autoFixture.Create<DataLockRequestRestartRequest>();
-
-            _dataLocksWithCourseMismatch = new List<DataLock>
+            new()
             {
-                new()
-                {
-                    IsResolved = false,
-                    DataLockStatus = Status.Fail,
-                    ErrorCode = DataLockErrorCode.Dlock04,
-                    IlrTrainingCourseCode = "454-3-1",
-                    IlrEffectiveFromDate = DateTime.Now.Date.AddDays(15)
-                }
-            };
-            _getDataLockSummariesResponse = autoFixture.Build<GetDataLockSummariesResponse>().With(x => x.DataLocksWithCourseMismatch, _dataLocksWithCourseMismatch).Create();
+                IsResolved = false,
+                DataLockStatus = Status.Fail,
+                ErrorCode = DataLockErrorCode.Dlock04,
+                IlrTrainingCourseCode = "454-3-1",
+                IlrEffectiveFromDate = DateTime.Now.Date.AddDays(15)
+            }
+        };
+        _getDataLockSummariesResponse = autoFixture.Build<GetDataLockSummariesResponse>().With(x => x.DataLocksWithCourseMismatch, _dataLocksWithCourseMismatch).Create();
             
-            _getApprenticeshipResponse = autoFixture.Build<GetApprenticeshipResponse>()
-                .With(p => p.CourseCode, "111")
-                .With(p => p.CourseName, "Training 111")
-                .With(p => p.EndDate, DateTime.Now.Date.AddDays(100))
-                .Create();
+        _getApprenticeshipResponse = autoFixture.Build<GetApprenticeshipResponse>()
+            .With(p => p.CourseCode, "111")
+            .With(p => p.CourseName, "Training 111")
+            .With(p => p.EndDate, DateTime.Now.Date.AddDays(100))
+            .Create();
 
 
-            _trainingProgrammes = new List<TrainingProgramme>
-            {
-                new() { CourseCode = "454-3-1", ProgrammeType = ProgrammeType.Standard, Name = "Training 111" }
-            };
-            _getAllTrainingProgrammesResponse = autoFixture.Build<GetAllTrainingProgrammesResponse>().With(x => x.TrainingProgrammes, _trainingProgrammes).Create();
-
-            _mockCommitmentsApiClient = new Mock<ICommitmentsApiClient>();
-            _mockCommitmentsApiClient.Setup(m => m.GetApprenticeshipDatalockSummariesStatus(It.IsAny<long>(), It.IsAny<CancellationToken>()))
-                .ReturnsAsync(_getDataLockSummariesResponse);
-            _mockCommitmentsApiClient.Setup(m => m.GetApprenticeship(It.IsAny<long>(), It.IsAny<CancellationToken>()))
-                .ReturnsAsync(_getApprenticeshipResponse);
-            _mockCommitmentsApiClient.Setup(m => m.GetAllTrainingProgrammes(It.IsAny<CancellationToken>()))
-               .ReturnsAsync(_getAllTrainingProgrammesResponse);
-
-            _mapper = new DataLockRequestRestartRequestToViewModelMapper(_mockCommitmentsApiClient.Object);
-        }   
-
-        [Test]
-        public async Task ApprenticeshipHashedId_IsMapped()
+        _trainingProgrammes = new List<TrainingProgramme>
         {
-            //Act
-            var result = await _mapper.Map(_dataLockRequestRestartRequest);
+            new() { CourseCode = "454-3-1", ProgrammeType = ProgrammeType.Standard, Name = "Training 111" }
+        };
+        _getAllTrainingProgrammesResponse = autoFixture.Build<GetAllTrainingProgrammesResponse>().With(x => x.TrainingProgrammes, _trainingProgrammes).Create();
 
-            //Assert
-            Assert.That(result.ApprenticeshipHashedId, Is.EqualTo(_dataLockRequestRestartRequest.ApprenticeshipHashedId));
-        }
+        _mockCommitmentsApiClient = new Mock<ICommitmentsApiClient>();
+        _mockCommitmentsApiClient.Setup(m => m.GetApprenticeshipDatalockSummariesStatus(It.IsAny<long>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(_getDataLockSummariesResponse);
+        _mockCommitmentsApiClient.Setup(m => m.GetApprenticeship(It.IsAny<long>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(_getApprenticeshipResponse);
+        _mockCommitmentsApiClient.Setup(m => m.GetAllTrainingProgrammes(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(_getAllTrainingProgrammesResponse);
 
-        [Test]
-        public async Task ApprenticeshipId_IsMapped()
-        {
-            //Act
-            var result = await _mapper.Map(_dataLockRequestRestartRequest);
+        _mapper = new DataLockRequestRestartRequestToViewModelMapper(_mockCommitmentsApiClient.Object);
+    }   
 
-            //Assert
-            Assert.That(result.ApprenticeshipId, Is.EqualTo(_dataLockRequestRestartRequest.ApprenticeshipId));
-        }
+    [Test]
+    public async Task ApprenticeshipHashedId_IsMapped()
+    {
+        //Act
+        var result = await _mapper.Map(_dataLockRequestRestartRequest);
 
-        [Test]
-        public async Task FirstName_IsMapped()
-        {
-            //Act
-            var result = await _mapper.Map(_dataLockRequestRestartRequest);
+        //Assert
+        result.ApprenticeshipHashedId.Should().Be(_dataLockRequestRestartRequest.ApprenticeshipHashedId);
+    }
 
-            //Assert
-            Assert.That(result.FirstName, Is.EqualTo(_getApprenticeshipResponse.FirstName));
-        }
+    [Test]
+    public async Task ApprenticeshipId_IsMapped()
+    {
+        //Act
+        var result = await _mapper.Map(_dataLockRequestRestartRequest);
 
-        [Test]
-        public async Task LastName_IsMapped()
-        {
-            //Act
-            var result = await _mapper.Map(_dataLockRequestRestartRequest);
+        //Assert
+        result.ApprenticeshipId.Should().Be(_dataLockRequestRestartRequest.ApprenticeshipId);
+    }
 
-            //Assert
-            Assert.That(result.LastName, Is.EqualTo(_getApprenticeshipResponse.LastName));
-        }
+    [Test]
+    public async Task FirstName_IsMapped()
+    {
+        //Act
+        var result = await _mapper.Map(_dataLockRequestRestartRequest);
 
-        [Test]
-        public async Task ULN_IsMapped()
-        {
-            //Act
-            var result = await _mapper.Map(_dataLockRequestRestartRequest);
+        //Assert
+        result.FirstName.Should().Be(_getApprenticeshipResponse.FirstName);
+    }
 
-            //Assert
-            Assert.That(result.ULN, Is.EqualTo(_getApprenticeshipResponse.Uln));
-        }
+    [Test]
+    public async Task LastName_IsMapped()
+    {
+        //Act
+        var result = await _mapper.Map(_dataLockRequestRestartRequest);
 
-        [Test]
-        public async Task CourseName_IsMapped()
-        {
-            //Act
-            var result = await _mapper.Map(_dataLockRequestRestartRequest);
+        //Assert
+        result.LastName.Should().Be(_getApprenticeshipResponse.LastName);
+    }
 
-            //Assert
-            Assert.That(result.CourseName, Is.EqualTo(_getApprenticeshipResponse.CourseName));
-        }
+    [Test]
+    public async Task ULN_IsMapped()
+    {
+        //Act
+        var result = await _mapper.Map(_dataLockRequestRestartRequest);
 
-        [Test]
-        public async Task ProviderId_IsMapped()
-        {
-            //Act
-            var result = await _mapper.Map(_dataLockRequestRestartRequest);
+        //Assert
+        result.ULN.Should().Be(_getApprenticeshipResponse.Uln);
+    }
 
-            //Assert
-            Assert.That(result.ProviderId, Is.EqualTo(_getApprenticeshipResponse.ProviderId));
-        }
+    [Test]
+    public async Task CourseName_IsMapped()
+    {
+        //Act
+        var result = await _mapper.Map(_dataLockRequestRestartRequest);
 
-        [Test]
-        public async Task ProviderName_IsMapped()
-        {
-            //Act
-            var result = await _mapper.Map(_dataLockRequestRestartRequest);
+        //Assert
+        result.CourseName.Should().Be(_getApprenticeshipResponse.CourseName);
+    }
 
-            //Assert
-            Assert.That(result.ProviderName, Is.EqualTo(_getApprenticeshipResponse.ProviderName));
-        }
+    [Test]
+    public async Task ProviderId_IsMapped()
+    {
+        //Act
+        var result = await _mapper.Map(_dataLockRequestRestartRequest);
 
-        [Test]
-        public async Task NewCourseCode_IsMapped()
-        {
-            //Act
-            var result = await _mapper.Map(_dataLockRequestRestartRequest);
+        //Assert
+        result.ProviderId.Should().Be(_getApprenticeshipResponse.ProviderId);
+    }
 
-            //Assert           
-            Assert.That(result.NewCourseCode, Is.EqualTo(_getAllTrainingProgrammesResponse.TrainingProgrammes.FirstOrDefault().CourseCode));
-        }
+    [Test]
+    public async Task ProviderName_IsMapped()
+    {
+        //Act
+        var result = await _mapper.Map(_dataLockRequestRestartRequest);
 
-        [Test]
-        public async Task NewCourseName_IsMapped()
-        {
-            //Act
-            var result = await _mapper.Map(_dataLockRequestRestartRequest);
+        //Assert
+        result.ProviderName.Should().Be(_getApprenticeshipResponse.ProviderName);
+    }
 
-            //Assert            
-            Assert.That(result.NewCourseName, Is.EqualTo(_getAllTrainingProgrammesResponse.TrainingProgrammes.FirstOrDefault().Name));
-        } 
+    [Test]
+    public async Task NewCourseCode_IsMapped()
+    {
+        //Act
+        var result = await _mapper.Map(_dataLockRequestRestartRequest);
 
-        [Test]
-        public async Task IlrEffectiveFromDate_IsMapped()
-        {
-            //Act
-            var result = await _mapper.Map(_dataLockRequestRestartRequest);
+        //Assert           
+        result.NewCourseCode.Should().Be(_getAllTrainingProgrammesResponse.TrainingProgrammes.FirstOrDefault().CourseCode);
+    }
 
-            //Assert            
-            Assert.That(result.IlrEffectiveFromDate, Is.EqualTo(_getDataLockSummariesResponse.DataLocksWithCourseMismatch.FirstOrDefault().IlrEffectiveFromDate));
-        }
+    [Test]
+    public async Task NewCourseName_IsMapped()
+    {
+        //Act
+        var result = await _mapper.Map(_dataLockRequestRestartRequest);
 
-        [Test]
-        public async Task IlrEffectiveToDate_IsMapped()
-        {
-            //Act
-            var result = await _mapper.Map(_dataLockRequestRestartRequest);
+        //Assert            
+        result.NewCourseName.Should().Be(_getAllTrainingProgrammesResponse.TrainingProgrammes.FirstOrDefault().Name);
+    } 
 
-            //Assert
-            Assert.That(result.IlrEffectiveToDate, Is.EqualTo(_getDataLockSummariesResponse.DataLocksWithCourseMismatch.FirstOrDefault().IlrPriceEffectiveToDate));
-        }
+    [Test]
+    public async Task IlrEffectiveFromDate_IsMapped()
+    {
+        //Act
+        var result = await _mapper.Map(_dataLockRequestRestartRequest);
 
-        [Test]
-        public async Task GetDraftApprenticeshipIsCalled()
-        {
-            //Act
-            var result = await _mapper.Map(_dataLockRequestRestartRequest);
+        //Assert            
+        result.IlrEffectiveFromDate.Should().Be(_getDataLockSummariesResponse.DataLocksWithCourseMismatch.FirstOrDefault().IlrEffectiveFromDate);
+    }
 
-            //Assert
-            _mockCommitmentsApiClient.Verify(m => m.GetApprenticeship(It.IsAny<long>(), It.IsAny<CancellationToken>()), Times.Once());
-        }
+    [Test]
+    public async Task IlrEffectiveToDate_IsMapped()
+    {
+        //Act
+        var result = await _mapper.Map(_dataLockRequestRestartRequest);
 
-        [Test]
-        public async Task GetApprenticeshipDataLockSummariesStatusIsCalled()
-        {
-            //Act
-            var result = await _mapper.Map(_dataLockRequestRestartRequest);
+        //Assert
+        result.IlrEffectiveToDate.Should().Be(_getDataLockSummariesResponse.DataLocksWithCourseMismatch.FirstOrDefault().IlrPriceEffectiveToDate);
+    }
 
-            //Assert
-            _mockCommitmentsApiClient.Verify(m => m.GetApprenticeshipDatalockSummariesStatus(It.IsAny<long>(), It.IsAny<CancellationToken>()), Times.Once());
-        }
+    [Test]
+    public async Task GetDraftApprenticeshipIsCalled()
+    {
+        //Act
+        var result = await _mapper.Map(_dataLockRequestRestartRequest);
 
-        [Test]
-        public async Task GetAllTrainingProgrammesIsCalled()
-        {
-            //Act
-            var result = await _mapper.Map(_dataLockRequestRestartRequest);
+        //Assert
+        _mockCommitmentsApiClient.Verify(m => m.GetApprenticeship(It.IsAny<long>(), It.IsAny<CancellationToken>()), Times.Once());
+    }
 
-            //Assert
-            _mockCommitmentsApiClient.Verify(m => m.GetAllTrainingProgrammes(It.IsAny<CancellationToken>()), Times.Once());
-        }
+    [Test]
+    public async Task GetApprenticeshipDataLockSummariesStatusIsCalled()
+    {
+        //Act
+        var result = await _mapper.Map(_dataLockRequestRestartRequest);
+
+        //Assert
+        _mockCommitmentsApiClient.Verify(m => m.GetApprenticeshipDatalockSummariesStatus(It.IsAny<long>(), It.IsAny<CancellationToken>()), Times.Once());
+    }
+
+    [Test]
+    public async Task GetAllTrainingProgrammesIsCalled()
+    {
+        //Act
+        var result = await _mapper.Map(_dataLockRequestRestartRequest);
+
+        //Assert
+        _mockCommitmentsApiClient.Verify(m => m.GetAllTrainingProgrammes(It.IsAny<CancellationToken>()), Times.Once());
     }
 }
--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Mappers/Apprentice/PriceViewModelMapperTests.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Mappers/Apprentice/PriceViewModelMapperTests.cs
@@ -6,82 +6,81 @@ using SFA.DAS.ProviderCommitments.Web.Mappers.Apprentice;
 using SFA.DAS.ProviderCommitments.Web.Models.Apprentice;
 using SFA.DAS.ProviderCommitments.Web.Services.Cache;
 
-namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Mappers.Apprentice
+namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Mappers.Apprentice;
+
+[TestFixture]
+public class PriceViewModelMapperTests
 {
-    [TestFixture]
-    public class PriceViewModelMapperTests
+    private PriceViewModelMapper _mapper;
+    private PriceRequest _source;
+    private Func<Task<PriceViewModel>> _act;
+    private Mock<ICommitmentsApiClient> _commitmentsApiClientMock;
+    private Mock<ICacheStorageService> _cacheStorage;
+    private GetApprenticeshipResponse _getApprenticeshipApiResponse;
+    private ChangeEmployerCacheItem _cacheItem;
+
+    [SetUp]
+    public void Arrange()
     {
-        private PriceViewModelMapper _mapper;
-        private PriceRequest _source;
-        private Func<Task<PriceViewModel>> _act;
-        private Mock<ICommitmentsApiClient> _commitmentsApiClientMock;
-        private Mock<ICacheStorageService> _cacheStorage;
-        private GetApprenticeshipResponse _getApprenticeshipApiResponse;
-        private ChangeEmployerCacheItem _cacheItem;
+        var fixture = new Fixture();
+        _source = fixture.Build<PriceRequest>().Create();
 
-        [SetUp]
-        public void Arrange()
-        {
-            var fixture = new Fixture();
-            _source = fixture.Build<PriceRequest>().Create();
+        _getApprenticeshipApiResponse = new GetApprenticeshipResponse { EmployerName = "TestName" };
 
-            _getApprenticeshipApiResponse = new GetApprenticeshipResponse { EmployerName = "TestName" };
+        _commitmentsApiClientMock = new Mock<ICommitmentsApiClient>();
+        _commitmentsApiClientMock
+            .Setup(x => x.GetApprenticeship(_source.ApprenticeshipId, default(CancellationToken)))
+            .ReturnsAsync(_getApprenticeshipApiResponse);
 
-            _commitmentsApiClientMock = new Mock<ICommitmentsApiClient>();
-            _commitmentsApiClientMock
-                .Setup(x => x.GetApprenticeship(_source.ApprenticeshipId, default(CancellationToken)))
-                .ReturnsAsync(_getApprenticeshipApiResponse);
-
-            _cacheStorage = new Mock<ICacheStorageService>();
-            _cacheItem = fixture
-                          .Build<ChangeEmployerCacheItem>()
-                          .With(x => x.StartDate, "092022")
-                          .With(x => x.EndDate, "092023")
-                          .With(x => x.EmploymentEndDate, string.Empty)
-                          .Create();
+        _cacheStorage = new Mock<ICacheStorageService>();
+        _cacheItem = fixture
+            .Build<ChangeEmployerCacheItem>()
+            .With(x => x.StartDate, "092022")
+            .With(x => x.EndDate, "092023")
+            .With(x => x.EmploymentEndDate, string.Empty)
+            .Create();
             
-            _cacheStorage.Setup(x =>
-                    x.RetrieveFromCache<ChangeEmployerCacheItem>(It.Is<Guid>(key => key == _source.CacheKey)))
-                .ReturnsAsync(_cacheItem);
+        _cacheStorage.Setup(x =>
+                x.RetrieveFromCache<ChangeEmployerCacheItem>(It.Is<Guid>(key => key == _source.CacheKey)))
+            .ReturnsAsync(_cacheItem);
 
-            _mapper = new PriceViewModelMapper(_commitmentsApiClientMock.Object, _cacheStorage.Object);
+        _mapper = new PriceViewModelMapper(_commitmentsApiClientMock.Object, _cacheStorage.Object);
 
-            _act = async () => await _mapper.Map(TestHelper.Clone(_source));
-        }
+        _act = async () => await _mapper.Map(TestHelper.Clone(_source));
+    }
 
-        [Test]
-        public async Task ThenProviderIdMappedCorrectly()
-        {
-            var result = await _act();
-            Assert.That(result.ProviderId, Is.EqualTo(_source.ProviderId));
-        }
+    [Test]
+    public async Task ThenProviderIdMappedCorrectly()
+    {
+        var result = await _act();
+        result.ProviderId.Should().Be(_source.ProviderId);
+    }
 
-        [Test]
-        public async Task ThenApprenticeshipHashedIdIsMappedCorrectly()
-        {
-            var result = await _act();
-            Assert.That(result.ApprenticeshipHashedId, Is.EqualTo(_source.ApprenticeshipHashedId));
-        }
+    [Test]
+    public async Task ThenApprenticeshipHashedIdIsMappedCorrectly()
+    {
+        var result = await _act();
+        result.ApprenticeshipHashedId.Should().Be(_source.ApprenticeshipHashedId);
+    }
 
-        [Test]
-        public async Task ThenLegalEntityNameIsMappedCorrectly()
-        {
-            var result = await _act();
-            Assert.That(result.LegalEntityName, Is.EqualTo(_getApprenticeshipApiResponse.EmployerName));
-        }
+    [Test]
+    public async Task ThenLegalEntityNameIsMappedCorrectly()
+    {
+        var result = await _act();
+        result.LegalEntityName.Should().Be(_getApprenticeshipApiResponse.EmployerName);
+    }
 
-        [Test]
-        public async Task ThenEditModeIsMappedCorrectly()
-        {
-            var result = await _act();
-            Assert.That(result.InEditMode, Is.EqualTo(_source.IsEdit));
-        }
+    [Test]
+    public async Task ThenEditModeIsMappedCorrectly()
+    {
+        var result = await _act();
+        result.InEditMode.Should().Be(_source.IsEdit);
+    }
 
-        [Test]
-        public async Task ThenApprenticeshipStatusIsMappedCorrectly()
-        {
-            var result = await _act();
-            Assert.That(result.ApprenticeshipStatus, Is.EqualTo(_getApprenticeshipApiResponse.Status));
-        }
+    [Test]
+    public async Task ThenApprenticeshipStatusIsMappedCorrectly()
+    {
+        var result = await _act();
+        result.ApprenticeshipStatus.Should().Be(_getApprenticeshipApiResponse.Status);
     }
 }
