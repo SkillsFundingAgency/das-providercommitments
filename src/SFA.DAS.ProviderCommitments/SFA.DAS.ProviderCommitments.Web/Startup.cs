f939f203a96047a289304c6a67b775ef43c400b1
ogrwill <will.ogrady@digital.education.gov.uk>
2024-11-12 12:49:48 +0000

    adding App insights and removing nlog

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Startup.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Startup.cs
@@ -1,6 +1,6 @@
 ﻿using AspNetCore.IServiceCollection.AddIUrlHelper;
 using FluentValidation;
-using Microsoft.Extensions.Logging.ApplicationInsights;
+using OpenTelemetry.Logs;
 using SFA.DAS.Provider.Shared.UI.Startup;
 using SFA.DAS.ProviderCommitments.Application.Commands.CreateCohort;
 using SFA.DAS.ProviderCommitments.Extensions;
@@ -32,10 +32,11 @@ public class Startup
         services.AddHttpContextAccessor();
 
         services.AddSingleton(_configuration);
+
         services.AddLogging(builder =>
         {
-            builder.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
-            builder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Information);
+            builder.AddFilter<OpenTelemetryLoggerProvider>(string.Empty, LogLevel.Information);
+            builder.AddFilter<OpenTelemetryLoggerProvider>("Microsoft", LogLevel.Information);
         });
 
         services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<CreateCohortHandler>());
@@ -90,7 +91,7 @@ public class Startup
         });
 
         services.AddHttpClient();
-        services.AddApplicationInsightsTelemetry();
+        services.AddOpenTelemetryRegistration(_configuration);
     }
 
     public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
