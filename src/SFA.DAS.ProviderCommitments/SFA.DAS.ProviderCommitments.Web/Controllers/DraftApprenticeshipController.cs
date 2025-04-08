8c28ab80e7f80ab6355d8831d0fe25de5da3db63
Jon Harris <jon.harris@fuitsu.com>
2025-04-08 09:11:09 +0100

    refactor to alter routing

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Controllers/DraftApprenticeshipController.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Controllers/DraftApprenticeshipController.cs
@@ -593,8 +593,8 @@ namespace SFA.DAS.ProviderCommitments.Web.Controllers
             var request = await _modelMapper.Map<UpdateDraftApprenticeshipApimRequest>(model);
 
             await _outerApiService.UpdateDraftApprenticeship(model.CohortId, model.DraftApprenticeshipId, request);
-
-            return RedirectToAction("Details", "Cohort", new { model.ProviderId, model.CohortReference });
+            
+            return RedirectToAction(nameof(RecognisePriorLearning), new { model.CohortReference,  model.DraftApprenticeshipId, model.ProviderId });
         }
 
         [HttpGet]
