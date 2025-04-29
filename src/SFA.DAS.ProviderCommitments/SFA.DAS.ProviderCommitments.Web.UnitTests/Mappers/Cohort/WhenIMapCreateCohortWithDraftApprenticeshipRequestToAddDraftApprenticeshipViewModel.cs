b2138fd2bff6efe9fbaad0274451867eea1bb26d
Paul Graham <paul.graham@coprime.co.uk>
2025-04-29 15:50:06 +0100

    fix failijg test

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Mappers/Cohort/WhenIMapCreateCohortWithDraftApprenticeshipRequestToAddDraftApprenticeshipViewModel.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Mappers/Cohort/WhenIMapCreateCohortWithDraftApprenticeshipRequestToAddDraftApprenticeshipViewModel.cs
@@ -126,7 +126,7 @@ public class WhenIMapCreateCohortWithDraftApprenticeshipRequestToAddDraftApprent
     public async Task Then_EndDate_IsMappedCorrectly()
     {
         var result = await _mapper.Map(_source);
-        result.EndDate.Date.Value.Date.Should().Be(_cacheItem.EndDate.Value.Date);
+        result.EndDate.Date.Value.Date.Should().Be(_cacheItem.EndDate.Value.Date.GetFirstDayOfMonth());
     }
 
     [Test]
