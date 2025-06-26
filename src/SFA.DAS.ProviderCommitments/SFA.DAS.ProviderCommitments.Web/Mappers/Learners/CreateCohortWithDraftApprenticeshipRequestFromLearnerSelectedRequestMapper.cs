3bb3bfb38d7ddb39dffff3b4de3457da6ef237c8
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2025-06-26 10:46:38 +0100

    tidy

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/Learners/CreateCohortWithDraftApprenticeshipRequestFromLearnerSelectedRequestMapper.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/Learners/CreateCohortWithDraftApprenticeshipRequestFromLearnerSelectedRequestMapper.cs
@@ -6,27 +6,22 @@ using SFA.DAS.ProviderCommitments.Web.Services.Cache;
 
 namespace SFA.DAS.ProviderCommitments.Web.Mappers.Learners;
 
-public class CreateCohortWithDraftApprenticeshipRequestFromLearnerSelectedRequestMapper : IMapper<Models.Cohort.LearnerSelectedRequest, CreateCohortWithDraftApprenticeshipRequest>
+public class CreateCohortWithDraftApprenticeshipRequestFromLearnerSelectedRequestMapper(
+    ICacheStorageService cacheStorage,
+    IOuterApiService outerApiService)
+    : IMapper<Models.Cohort.LearnerSelectedRequest, CreateCohortWithDraftApprenticeshipRequest>
 {
-    private readonly ICacheStorageService _cacheStorage;
-    private readonly IOuterApiService _outerApiService;
-
-    public CreateCohortWithDraftApprenticeshipRequestFromLearnerSelectedRequestMapper(ICacheStorageService cacheStorage, IOuterApiService outerApiService)
-    {
-        _cacheStorage = cacheStorage;
-        _outerApiService = outerApiService;
-    }
-
     public async Task<CreateCohortWithDraftApprenticeshipRequest> Map(Models.Cohort.LearnerSelectedRequest source)
     {
-        var learner = await _outerApiService.GetLearnerSelected(source.ProviderId, source.LearnerDataId);
+        var learner = await outerApiService.GetLearnerSelected(source.ProviderId, source.LearnerDataId);
 
-        var cacheItem = await _cacheStorage.RetrieveFromCache<CreateCohortCacheItem>(source.CacheKey);
+        var cacheItem = await cacheStorage.RetrieveFromCache<CreateCohortCacheItem>(source.CacheKey);
         cacheItem.FirstName = learner.FirstName;
         cacheItem.LastName = learner.LastName;
         cacheItem.Email = learner.Email;
         cacheItem.DateOfBirth = learner.Dob;
         cacheItem.StartDate = learner.StartDate;
+        cacheItem.ActualStartDate = learner.StartDate;
         cacheItem.EndDate = learner.PlannedEndDate;
         cacheItem.DeliveryModel = learner.IsFlexiJob ? DeliveryModel.FlexiJobAgency : DeliveryModel.Regular;
         cacheItem.Uln = learner.Uln.ToString();
@@ -35,7 +30,7 @@ public class CreateCohortWithDraftApprenticeshipRequestFromLearnerSelectedReques
         cacheItem.CourseCode = learner.StandardCode.ToString();
         cacheItem.Cost = learner.TrainingPrice + learner.EpaoPrice;
         cacheItem.LearnerDataId = source.LearnerDataId;
-        await _cacheStorage.SaveToCache(cacheItem.CacheKey, cacheItem, 1);
+        await cacheStorage.SaveToCache(cacheItem.CacheKey, cacheItem, 1);
 
         return new CreateCohortWithDraftApprenticeshipRequest
         {
