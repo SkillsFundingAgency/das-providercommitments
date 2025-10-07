b7e70e94a00a3337c513f0aa4f92f927773dc3e0
KUMAR, Santhosh <Santhosh.KUMAR@EDUCATION.GOV.UK>
2025-10-07 16:23:18 +0100

    refactored code

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Infrastructure/OuterApi/Requests/Provider/GetProviderDetailsRequest.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Infrastructure/OuterApi/Requests/Provider/GetProviderDetailsRequest.cs
@@ -1,20 +1,13 @@
-﻿namespace SFA.DAS.ProviderCommitments.Infrastructure.OuterApi.Requests.Provider
+﻿namespace SFA.DAS.ProviderCommitments.Infrastructure.OuterApi.Requests.Provider;
+
+public class GetProviderDetailsRequest(long ukprn) : IGetApiRequest
 {
-    public class GetProviderDetailsRequest : IGetApiRequest
-    {
-        private readonly long _ukprn;
+    public long _ukprn { get; } = ukprn;
 
-        public GetProviderDetailsRequest(long ukprn)
-        {
-            _ukprn = ukprn;
-        }
+    public string GetUrl => $"provideraccounts/providerStatus/{_ukprn}";
+}
 
-        public string GetUrl => $"provideraccounts/providerStatus/{_ukprn}";
-    }
-
-    public class GetProviderDetailsResponse
-    {
-        public int ProviderStatusTypeId { get; set; }
-    }
-
+public class GetProviderDetailsResponse(int providerStatusTypeId)
+{
+    public int ProviderStatusTypeId { get; } = providerStatusTypeId;
 }
