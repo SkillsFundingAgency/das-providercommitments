972686a7dbfa16c4219e2bc7ce29fe8f42c294ed
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2025-12-19 13:10:36 +0000

    return the taks directly to clean Client code up

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Infrastructure/ApprovalsOuterApiClient.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Infrastructure/ApprovalsOuterApiClient.cs
@@ -1,4 +1,4 @@
-﻿using SFA.DAS.ProviderCommitments.Infrastructure.OuterApi.Requests.Cohorts;
+using SFA.DAS.ProviderCommitments.Infrastructure.OuterApi.Requests.Cohorts;
 using SFA.DAS.ProviderCommitments.Infrastructure.OuterApi.Responses.Account;
 using SFA.DAS.ProviderCommitments.Infrastructure.OuterApi.Responses.ProviderRelationships;
 using SFA.DAS.ProviderCommitments.Interfaces;
@@ -7,45 +7,40 @@ namespace SFA.DAS.ProviderCommitments.Infrastructure
 {
     public class ApprovalsOuterApiClient(IApprovalsOuterApiHttpClient client) : IApprovalsOuterApiClient
     {
-        public async Task<ProviderCourseDeliveryModels> GetProviderCourseDeliveryModels(
+        public Task<ProviderCourseDeliveryModels> GetProviderCourseDeliveryModels(
             long providerId,
             string courseCode,
             long accountLegalEntityId,
             CancellationToken cancellationToken = default)
         {
-            return await client.Get<ProviderCourseDeliveryModels>
+            return client.Get<ProviderCourseDeliveryModels>
                 ($"providers/{providerId}/courses?trainingCode={courseCode}&accountLegalEntityId={accountLegalEntityId}",
                 cancellationToken: cancellationToken);
         }
 
-        public async Task<GetHasPermissionResponse> GetHasPermission(long ukprn, long accountLegalEntityId)
+        public Task<GetHasPermissionResponse> GetHasPermission(long ukprn, long accountLegalEntityId)
         {
-            var result = await client.Get<GetHasPermissionResponse>($"providerpermissions/has-relationship-with-permission?ukprn={ukprn}&accountLegalEntityId={accountLegalEntityId}");
-            return result;
+            return client.Get<GetHasPermissionResponse>($"providerpermissions/has-relationship-with-permission?ukprn={ukprn}&accountLegalEntityId={accountLegalEntityId}");
         }
 
-        public async Task<GetHasRelationshipWithPermissionResponse> GetHasRelationshipWithPermission(long ukprn)
+        public Task<GetHasRelationshipWithPermissionResponse> GetHasRelationshipWithPermission(long ukprn)
         {
-            var result = await client.Get<GetHasRelationshipWithPermissionResponse>($"providerpermissions/has-relationship-with-permission?ukprn={ukprn}");
-            return result;
+            return client.Get<GetHasRelationshipWithPermissionResponse>($"providerpermissions/has-relationship-with-permission?ukprn={ukprn}");
         }
 
-        public async Task<GetProviderAccountLegalEntitiesResponse> GetProviderAccountLegalEntities(int ukprn)
+        public Task<GetProviderAccountLegalEntitiesResponse> GetProviderAccountLegalEntities(int ukprn)
         {
-            var result = await client.Get<GetProviderAccountLegalEntitiesResponse>($"providerpermissions/account-provider-legal-entities?ukprn={ukprn}");
-            return result;
+            return client.Get<GetProviderAccountLegalEntitiesResponse>($"providerpermissions/account-provider-legal-entities?ukprn={ukprn}");
         }
 
-        public async Task<GetAccountResponse> GetAccount(string hashedAccountId)
+        public Task<GetAccountResponse> GetAccount(string hashedAccountId)
         {
-            var result = await client.Get<GetAccountResponse>($"accounts/{hashedAccountId}");
-            return result;
+            return client.Get<GetAccountResponse>($"accounts/{hashedAccountId}");
         }
 
-        public async Task<GetSelectEmployerResponse> GetSelectEmployer(GetSelectEmployerRequest request)
+        public Task<GetSelectEmployerResponse> GetSelectEmployer(GetSelectEmployerRequest request)
         {
-            var result = await client.Get<GetSelectEmployerResponse>(request.GetUrl);
-            return result;
+            return client.Get<GetSelectEmployerResponse>(request.GetUrl);
         }
     }
 }
