75b6b5ef9d0018c47c0ed7967ad6a6fed47d1631
KUMAR, Santhosh <Santhosh.KUMAR@EDUCATION.GOV.UK>
2026-01-06 11:48:02 +0000

    Refactored code

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Infrastructure/OuterApi/Requests/DraftApprenticeship/DraftApprenticeAddEmailRequest.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Infrastructure/OuterApi/Requests/DraftApprenticeship/DraftApprenticeAddEmailRequest.cs
@@ -1,10 +1,10 @@
 ﻿namespace SFA.DAS.ProviderCommitments.Infrastructure.OuterApi.Requests.DraftApprenticeship;
 
-public class DraftApprenticeAddEmailRequest(long providerId, long cohortId, long draftApprenticeshipId) : IPostApiRequest
+public class DraftApprenticeAddEmailRequest(long providerId, long cohortId, long draftApprenticeshipId) : IPutApiRequest
 {
     public long CohortId { get; set; } = cohortId;
     public long DraftApprenticeshipId { get; set; } = draftApprenticeshipId;
     public long ProviderId { get; set; } = providerId;
-    public string PostUrl => $"provider/{ProviderId}/unapproved/{CohortId}/apprentices/{DraftApprenticeshipId}/email";
+    public string PutUrl => $"provider/{ProviderId}/unapproved/{CohortId}/apprentices/{DraftApprenticeshipId}/email";
     public object Data { get; set; }
 }
--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Infrastructure/OuterApi/Requests/DraftApprenticeship/PostDraftApprenticeshipSetReferenceRequest.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Infrastructure/OuterApi/Requests/DraftApprenticeship/PostDraftApprenticeshipSetReferenceRequest.cs
@@ -1,14 +1,12 @@
-﻿using SFA.DAS.CommitmentsV2.Types;
-
-namespace SFA.DAS.ProviderCommitments.Infrastructure.OuterApi.Requests.DraftApprenticeship;
+﻿namespace SFA.DAS.ProviderCommitments.Infrastructure.OuterApi.Requests.DraftApprenticeship;
 
 public class PostDraftApprenticeshipSetReferenceRequest(long providerId, long cohortId, long draftApprenticeshipId)
-        : IPostApiRequest
+        : IPutApiRequest
 {
     public long CohortId { get; set; } = cohortId;
     public long DraftApprenticeshipId { get; set; } = draftApprenticeshipId;
     public long ProviderId { get; set; } = providerId;
     public object Data { get; set; }
 
-    public string PostUrl => $"provider/{ProviderId}/unapproved/{CohortId}/apprentices/{DraftApprenticeshipId}/reference";
+    public string PutUrl => $"provider/{ProviderId}/unapproved/{CohortId}/apprentices/{DraftApprenticeshipId}/reference";
 }
