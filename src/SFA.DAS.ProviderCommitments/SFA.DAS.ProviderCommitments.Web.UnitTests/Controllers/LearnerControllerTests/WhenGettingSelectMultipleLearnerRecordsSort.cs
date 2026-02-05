7e503d6fbe3530875fb45afe298f5801bf415ce2
Witek <witek.poreda@education.gov.uk>
2026-02-05 15:37:02 +0000

    add tests

---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Controllers/LearnerControllerTests/WhenGettingSelectMultipleLearnerRecordsSort.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Routing;
+using SFA.DAS.CommitmentsV2.Shared.Interfaces;
+using SFA.DAS.ProviderCommitments.Web.Controllers;
+using SFA.DAS.ProviderCommitments.Web.Models.Cohort;
+
+namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Controllers.LearnerControllerTests;
+
+[TestFixture]
+public class WhenGettingSelectMultipleLearnerRecordsSort
+{
+    [Test]
+    public async Task ThenCallsModelMapper()
+    {
+        var fixture = new GetSelectMultipleLearnerRecordsSortFixture();
+
+        await fixture.Act();
+
+        fixture.VerifyMapperWasCalled();
+    }
+
+    [Test]
+    public async Task ThenRedirectsToSelectMultipleLearnerRecords_WithMappedRouteValues()
+    {
+        var fixture = new GetSelectMultipleLearnerRecordsSortFixture();
+
+        var result = await fixture.Act();
+
+        var redirect = result.Should().BeOfType<RedirectToActionResult>().Subject;
+        redirect.ActionName.Should().Be("SelectMultipleLearnerRecords");
+        redirect.ControllerName.Should().BeNull(); // same controller
+        redirect.RouteValues.Should().BeEquivalentTo(fixture.ExpectedRouteValues);
+    }
+}
+
+public class GetSelectMultipleLearnerRecordsSortFixture
+{
+    private readonly LearnerController _sut;
+    private readonly Mock<IModelMapper> _modelMapperMock;
+    private readonly SelectMultipleLearnerRecordsSortRequest _request;
+    private readonly SelectMultipleLearnerRecordsRequest _redirectRequest;
+
+    public RouteValueDictionary ExpectedRouteValues { get; }
+
+    public GetSelectMultipleLearnerRecordsSortFixture()
+    {
+        var fixture = new Fixture();
+
+        _modelMapperMock = new Mock<IModelMapper>();
+
+        _request = fixture.Create<SelectMultipleLearnerRecordsSortRequest>();
+        _redirectRequest = fixture.Create<SelectMultipleLearnerRecordsRequest>();
+
+        ExpectedRouteValues = new RouteValueDictionary(_redirectRequest);
+
+        _modelMapperMock
+            .Setup(x => x.Map<SelectMultipleLearnerRecordsRequest>(_request))
+            .ReturnsAsync(_redirectRequest);
+
+        _sut = new LearnerController(_modelMapperMock.Object);
+    }
+
+    public Task<IActionResult> Act() => _sut.SelectMultipleLearnerRecordsSort(_request);
+
+    public void VerifyMapperWasCalled()
+        => _modelMapperMock.Verify(x => x.Map<SelectMultipleLearnerRecordsRequest>(_request), Times.Once);
+}
---
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web.UnitTests/Mappers/Learners/SelectMultipleLearnerRecordsViewModelMapperTests.cs
@@ -0,0 +1,126 @@
+﻿using System.Linq;
+using SFA.DAS.ProviderCommitments.Infrastructure.OuterApi.Requests.Ilr;
+using SFA.DAS.ProviderCommitments.Interfaces;
+using SFA.DAS.ProviderCommitments.Web.Mappers.Learners;
+using SFA.DAS.ProviderCommitments.Web.Models.Cohort;
+using SFA.DAS.ProviderCommitments.Web.Services.Cache;
+
+namespace SFA.DAS.ProviderCommitments.Web.UnitTests.Mappers.Learners;
+
+[TestFixture]
+public class SelectMultipleLearnerRecordsViewModelMapperTests
+{
+    private SelectMultipleLearnerRecordsViewModelMapper _mapper;
+    private Mock<IOuterApiService> _outerApiService;
+    private Mock<ICacheStorageService> _cacheStorage;
+
+    private SelectMultipleLearnerRecordsRequest _request;
+    private SelectMultipleLearnerRecordsCacheItem _cacheItem;
+    private GetLearnerDetailsForProviderResponse _apiResponse;
+
+    [SetUp]
+    public void Setup()
+    {
+        var fixture = new Fixture();
+
+        _request = fixture.Create<SelectMultipleLearnerRecordsRequest>();
+
+        _cacheItem = fixture.Build<SelectMultipleLearnerRecordsCacheItem>()
+            .With(x => x.StartMonth, "1")
+            .With(x => x.StartYear, "2025")
+            .Create();
+
+        _apiResponse = fixture.Create<GetLearnerDetailsForProviderResponse>();
+
+        _outerApiService = new Mock<IOuterApiService>();
+        _cacheStorage = new Mock<ICacheStorageService>();
+
+        _cacheStorage
+            .Setup(x => x.RetrieveFromCache<SelectMultipleLearnerRecordsCacheItem>(_request.CacheKey.Value))
+            .ReturnsAsync(_cacheItem);
+
+        _outerApiService
+            .Setup(x => x.GetLearnerDetailsForProvider(
+                _cacheItem.ProviderId,
+                _cacheItem.AccountLegalEntityId,
+                _cacheItem.CohortId,
+                _cacheItem.SearchTerm,
+                _cacheItem.SortField,
+                _cacheItem.ReverseSort,
+                _request.Page,
+                1,
+                2025))
+            .ReturnsAsync(_apiResponse);
+
+        _mapper = new SelectMultipleLearnerRecordsViewModelMapper(_outerApiService.Object, _cacheStorage.Object);
+    }
+
+    [Test]
+    public async Task MapToFilterModelCorrectly()
+    {
+        var result = await _mapper.Map(_request);
+
+        result.FilterModel.ProviderId.Should().Be(_cacheItem.ProviderId);
+        result.FilterModel.EmployerAccountLegalEntityPublicHashedId.Should().Be(_cacheItem.EmployerAccountLegalEntityPublicHashedId);
+        result.FilterModel.CohortReference.Should().Be(_cacheItem.CohortReference);
+
+        result.FilterModel.CacheKey.Should().Be(_cacheItem.CacheKey);
+
+        result.FilterModel.TotalNumberOfLearnersFound.Should().Be(_apiResponse.Total);
+        result.FilterModel.PageNumber.Should().Be(_request.Page);
+
+        result.FilterModel.SortField.Should().Be(_cacheItem.SortField);
+        result.FilterModel.ReverseSort.Should().Be(_cacheItem.ReverseSort);
+        result.FilterModel.SearchTerm.Should().Be(_cacheItem.SearchTerm);
+
+        result.FilterModel.StartMonth.Should().Be(_cacheItem.StartMonth);
+        result.FilterModel.StartYear.Should().Be(_cacheItem.StartYear);
+    }
+
+    [Test]
+    public async Task MapToViewModelCorrectly()
+    {
+        var result = await _mapper.Map(_request);
+
+        result.ProviderId.Should().Be(_cacheItem.ProviderId);
+        result.CohortReference.Should().Be(_cacheItem.CohortReference);
+        result.EmployerAccountLegalEntityPublicHashedId.Should().Be(_cacheItem.EmployerAccountLegalEntityPublicHashedId);
+        result.CacheKey.Should().Be(_request.CacheKey);
+        result.EmployerAccountName.Should().Be(_cacheItem.EmployerAccountName);
+        result.LastIlrSubmittedOn.Should().Be(_apiResponse.LastSubmissionDate);
+        result.FutureMonths.Should().Be(_apiResponse.FutureMonths);
+    }
+
+    [Test]
+    public async Task MapLearnersCorrectly()
+    {
+        var result = await _mapper.Map(_request);
+
+        var expected = _apiResponse.Learners.Select(x => (LearnerSummary)x).ToList();
+        result.Learners.Should().BeEquivalentTo(expected);
+
+    }
+
+    [Test]
+    public async Task Map_CallsDependenciesWithExpectedArguments()
+    {
+        await _mapper.Map(_request);
+
+        _cacheStorage.Verify(x =>
+            x.RetrieveFromCache<SelectMultipleLearnerRecordsCacheItem>(_request.CacheKey.Value),
+            Times.Once);
+
+        _outerApiService.Verify(x =>
+            x.GetLearnerDetailsForProvider(
+                _cacheItem.ProviderId,
+                _cacheItem.AccountLegalEntityId,
+                _cacheItem.CohortId,
+                _cacheItem.SearchTerm,
+                _cacheItem.SortField,
+                _cacheItem.ReverseSort,
+                _request.Page,
+                1,
+                2025),
+            Times.Once);
+    }
+}
