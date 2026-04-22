1800bfeeae0edadca75dac182d7b728f78d4ad52
skumardfe <Santhosh.KUMAR@education.gov.uk>
2026-04-22 13:01:19 +0100

    format code

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/ViewStandardOptionsViewModelMapper.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/ViewStandardOptionsViewModelMapper.cs
@@ -11,36 +11,35 @@ public class ViewStandardOptionsViewModelMapper : IMapper<SelectOptionsRequest, 
     private readonly ICommitmentsApiClient _commitmentsApiClient;
     private readonly IOuterApiClient _outerApiClient;
 
-        public ViewStandardOptionsViewModelMapper(ICommitmentsApiClient commitmentsApiClient, IOuterApiClient outerApiClient)
-        {
-            _commitmentsApiClient = commitmentsApiClient;
-            _outerApiClient = outerApiClient;
-        }
+    public ViewStandardOptionsViewModelMapper(ICommitmentsApiClient commitmentsApiClient, IOuterApiClient outerApiClient)
+    {
+        _commitmentsApiClient = commitmentsApiClient;
+        _outerApiClient = outerApiClient;
+    }
 
-        public async Task<ViewSelectOptionsViewModel> Map(SelectOptionsRequest source)
-        {
-            var apiRequest = new GetEditDraftApprenticeshipRequest(source.ProviderId, (long)source.CohortId, (long)source.DraftApprenticeshipId, null);
-            var draftApprenticeship = await _outerApiClient.Get<GetEditDraftApprenticeshipResponse>(apiRequest);
+    public async Task<ViewSelectOptionsViewModel> Map(SelectOptionsRequest source)
+    {
+        var apiRequest = new GetEditDraftApprenticeshipRequest(source.ProviderId, (long)source.CohortId, (long)source.DraftApprenticeshipId, null);
+        var draftApprenticeship = await _outerApiClient.Get<GetEditDraftApprenticeshipResponse>(apiRequest);
 
         var trainingProgramme = await _commitmentsApiClient.GetTrainingProgrammeVersionByStandardUId(draftApprenticeship.StandardUId);
 
-            return new ViewSelectOptionsViewModel
-            {
-                CohortId = source.CohortId,
-                CohortReference = source.CohortReference,
-                ProviderId = source.ProviderId,
-                DraftApprenticeshipId = source.DraftApprenticeshipId,
-                DraftApprenticeshipHashedId = source.DraftApprenticeshipHashedId,
-                TrainingCourseName = draftApprenticeship.CourseName,
-                TrainingCourseVersion = draftApprenticeship.TrainingCourseVersion,
-                Options = trainingProgramme.TrainingProgramme.Options != null ? trainingProgramme.TrainingProgramme.Options.ToList() : new List<string>(),
-                StandardPageUrl = trainingProgramme.TrainingProgramme.StandardPageUrl,
-                SelectedOption = draftApprenticeship.TrainingCourseOption == string.Empty ? "-1" : draftApprenticeship.TrainingCourseOption,
-                HasSelectedRpl = draftApprenticeship.RecognisePriorLearning,
-                ApprenticeshipStartDate = draftApprenticeship.StartDate,
-                LearnerDataId = draftApprenticeship.LearnerDataId,
-                OriginalSelectedOption = draftApprenticeship.TrainingCourseOption == string.Empty ? "-1" : draftApprenticeship.TrainingCourseOption,
-            };
-        }
+        return new ViewSelectOptionsViewModel
+        {
+            CohortId = source.CohortId,
+            CohortReference = source.CohortReference,
+            ProviderId = source.ProviderId,
+            DraftApprenticeshipId = source.DraftApprenticeshipId,
+            DraftApprenticeshipHashedId = source.DraftApprenticeshipHashedId,
+            TrainingCourseName = draftApprenticeship.CourseName,
+            TrainingCourseVersion = draftApprenticeship.TrainingCourseVersion,
+            Options = trainingProgramme.TrainingProgramme.Options != null ? trainingProgramme.TrainingProgramme.Options.ToList() : new List<string>(),
+            StandardPageUrl = trainingProgramme.TrainingProgramme.StandardPageUrl,
+            SelectedOption = draftApprenticeship.TrainingCourseOption == string.Empty ? "-1" : draftApprenticeship.TrainingCourseOption,
+            HasSelectedRpl = draftApprenticeship.RecognisePriorLearning,
+            ApprenticeshipStartDate = draftApprenticeship.StartDate,
+            LearnerDataId = draftApprenticeship.LearnerDataId,
+            OriginalSelectedOption = draftApprenticeship.TrainingCourseOption == string.Empty ? "-1" : draftApprenticeship.TrainingCourseOption,
+        };
     }
 }
