f94e742e510fd6a325a1ac94672675b02ca1ffd0
skumardfe <Santhosh.KUMAR@education.gov.uk>
2026-07-16 15:47:59 +0100

    add logs

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/Apprentice/IndexViewModelMapper.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments.Web/Mappers/Apprentice/IndexViewModelMapper.cs
@@ -10,34 +10,42 @@ public class IndexViewModelMapper : IMapper<IndexRequest, IndexViewModel>
 {
     private readonly IModelMapper _modelMapper;
     private readonly IOuterApiService _outerApiService;
+    private readonly ILogger<IndexViewModelMapper> _logger;
 
-    public IndexViewModelMapper(IModelMapper modelMapper, IOuterApiService outerApiService)
+    public IndexViewModelMapper(IModelMapper modelMapper, IOuterApiService outerApiService, ILogger<IndexViewModelMapper> logger)
     {
         _modelMapper = modelMapper;
         _outerApiService = outerApiService;
+        _logger = logger;
     }
 
     public async Task<IndexViewModel> Map(IndexRequest source)
     {
-        var response = await _outerApiService.GetApprenticeships(new
-            GetApprenticeshipsRequest(
-            source.ProviderId,
-            source.PageNumber,
-            Constants.ApprenticesSearch.NumberOfApprenticesPerSearchPage,
-            source.SortField,
-            source.ReverseSort,
-            source.SearchTerm,
-            source.SelectedEmployer, null,
-            source.SelectedCourse,
-            source.SelectedStatus,
-            source.SelectedStartDate,
-            source.SelectedEndDate, null, null, null,
-            source.SelectedAlert,
-            source.SelectedApprenticeConfirmation,
-            source.SelectedDeliveryModel));
-
-        var statusFilters = new[]
+        try
         {
+            _logger.LogInformation("Inside Index view model mapper");
+
+            var response = await _outerApiService.GetApprenticeships(new
+                GetApprenticeshipsRequest(
+                source.ProviderId,
+                source.PageNumber,
+                Constants.ApprenticesSearch.NumberOfApprenticesPerSearchPage,
+                source.SortField,
+                source.ReverseSort,
+                source.SearchTerm,
+                source.SelectedEmployer, null,
+                source.SelectedCourse,
+                source.SelectedStatus,
+                source.SelectedStartDate,
+                source.SelectedEndDate, null, null, null,
+                source.SelectedAlert,
+                source.SelectedApprenticeConfirmation,
+                source.SelectedDeliveryModel));
+
+            _logger.LogInformation("Response is null {a}", response is null);
+
+            var statusFilters = new[]
+            {
             ApprenticeshipStatus.WaitingToStart,
             ApprenticeshipStatus.Live,
             ApprenticeshipStatus.Paused,
@@ -45,58 +53,67 @@ public class IndexViewModelMapper : IMapper<IndexRequest, IndexViewModel>
             ApprenticeshipStatus.Completed
         };
 
-        var alertFilters = new[]
-        {
+            var alertFilters = new[]
+            {
             Alerts.ChangesForReview ,
             Alerts.ChangesPending,
             Alerts.ChangesRequested,
             Alerts.IlrDataMismatch,
         };
 
-        var filterModel = new ApprenticesFilterModel
-        {
-            ProviderId = source.ProviderId,
-            TotalNumberOfApprenticeships = response.TotalApprenticeships,
-            TotalNumberOfApprenticeshipsFound = response.TotalApprenticeshipsFound,
-            TotalNumberOfApprenticeshipsWithAlertsFound = response.TotalApprenticeshipsWithAlertsFound,
-            PageNumber = response.PageNumber,
-            SortField = source.SortField,
-            ReverseSort = source.ReverseSort,
-            SearchTerm = source.SearchTerm,
-            SelectedEmployer = source.SelectedEmployer,
-            SelectedCourse = source.SelectedCourse,
-            SelectedStatus = source.SelectedStatus,
-            SelectedStartDate = source.SelectedStartDate,
-            SelectedEndDate = source.SelectedEndDate,
-            SelectedAlert = source.SelectedAlert,
-            SelectedApprenticeConfirmation = source.SelectedApprenticeConfirmation,
-            SelectedDeliveryModel = source.SelectedDeliveryModel,
-            StatusFilters = statusFilters,
-            AlertFilters = alertFilters
-        };
+            var filterModel = new ApprenticesFilterModel
+            {
+                ProviderId = source.ProviderId,
+                TotalNumberOfApprenticeships = response.TotalApprenticeships,
+                TotalNumberOfApprenticeshipsFound = response.TotalApprenticeshipsFound,
+                TotalNumberOfApprenticeshipsWithAlertsFound = response.TotalApprenticeshipsWithAlertsFound,
+                PageNumber = response.PageNumber,
+                SortField = source.SortField,
+                ReverseSort = source.ReverseSort,
+                SearchTerm = source.SearchTerm,
+                SelectedEmployer = source.SelectedEmployer,
+                SelectedCourse = source.SelectedCourse,
+                SelectedStatus = source.SelectedStatus,
+                SelectedStartDate = source.SelectedStartDate,
+                SelectedEndDate = source.SelectedEndDate,
+                SelectedAlert = source.SelectedAlert,
+                SelectedApprenticeConfirmation = source.SelectedApprenticeConfirmation,
+                SelectedDeliveryModel = source.SelectedDeliveryModel,
+                StatusFilters = statusFilters,
+                AlertFilters = alertFilters
+            };
 
-        if (response.TotalApprenticeships >= Constants.ApprenticesSearch.NumberOfApprenticesRequiredForSearch)
+            if (response.TotalApprenticeships >= Constants.ApprenticesSearch.NumberOfApprenticesRequiredForSearch)
+            {
+                filterModel.EmployerFilters = response.ApprenticeshipFiltersValue.EmployerNames;
+                filterModel.CourseFilters = response.ApprenticeshipFiltersValue.CourseNames;
+                filterModel.StartDateFilters = response.ApprenticeshipFiltersValue.StartDates;
+                filterModel.EndDateFilters = response.ApprenticeshipFiltersValue.EndDates;
+            }
+
+            var apprenticeships = new List<ApprenticeshipDetailsViewModel>();
+
+            foreach (var apprenticeshipDetailsResponse in response.Apprenticeships)
+            {
+                var apprenticeship = await _modelMapper.Map<ApprenticeshipDetailsViewModel>(apprenticeshipDetailsResponse);
+                apprenticeships.Add(apprenticeship);
+            }
+
+            _logger.LogInformation("Processed successfully");
+
+            return new IndexViewModel
+            {
+                ProviderId = source.ProviderId,
+                Apprenticeships = apprenticeships,
+                FilterModel = filterModel,
+                HasChangeHistory = response.HasChangeHistory
+            };
+        }
+        catch (Exception e)
         {
-            filterModel.EmployerFilters = response.ApprenticeshipFiltersValue.EmployerNames;
-            filterModel.CourseFilters = response.ApprenticeshipFiltersValue.CourseNames;
-            filterModel.StartDateFilters = response.ApprenticeshipFiltersValue.StartDates;
-            filterModel.EndDateFilters = response.ApprenticeshipFiltersValue.EndDates;
+            _logger.LogError(e.Message);
         }
 
-        var apprenticeships = new List<ApprenticeshipDetailsViewModel>();
-
-        foreach (var apprenticeshipDetailsResponse in response.Apprenticeships)
-        {
-            var apprenticeship = await _modelMapper.Map<ApprenticeshipDetailsViewModel>(apprenticeshipDetailsResponse);
-            apprenticeships.Add(apprenticeship);
-        }
-
-        return new IndexViewModel
-        {
-            ProviderId = source.ProviderId,
-            Apprenticeships = apprenticeships,
-            FilterModel = filterModel,
-            HasChangeHistory = response.HasChangeHistory
-        };
+        return new IndexViewModel();
     }
 }
