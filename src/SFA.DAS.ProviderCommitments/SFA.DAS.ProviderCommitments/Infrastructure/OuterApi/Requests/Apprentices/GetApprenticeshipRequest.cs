c490a32f4ced16cc67c7782ba23ac5eb1a01f53f
skumardfe <Santhosh.KUMAR@education.gov.uk>
2026-08-06 16:46:56 +0100

    update url

--- src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Infrastructure/OuterApi/Requests/Apprentices/GetApprenticeshipRequest.cs
+++ src/SFA.DAS.ProviderCommitments/SFA.DAS.ProviderCommitments/Infrastructure/OuterApi/Requests/Apprentices/GetApprenticeshipRequest.cs
@@ -11,5 +11,5 @@ public class GetApprenticeshipRequest : IGetApiRequest
         _providerId = providerId;
     }
 
-    public string GetUrl => $"/provider/{_providerId}/apprentices/{_apprenticeshipId}";
+    public string GetUrl => $"provider/{_providerId}/apprentices/{_apprenticeshipId}";
 }
